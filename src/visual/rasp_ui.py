14b90028b6d889bbf4ff3519490144b6eb82e177
hawkwings <hawkwings@users.noreply.github.com>
2018-12-13 15:40:40 -0500

    Add UI

---
+++ src/visual/rasp_ui.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'rasp_ui.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalWidget.setGeometry(QtCore.QRect(90, 20, 601, 261))
+        self.horizontalWidget.setObjectName("horizontalWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.frame = QtWidgets.QFrame(self.horizontalWidget)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.pushButton = QtWidgets.QPushButton(self.frame)
+        self.pushButton.setGeometry(QtCore.QRect(180, 100, 115, 81))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.frame)
+        self.pushButton_2.setGeometry(QtCore.QRect(40, 100, 115, 81))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.frame)
+        self.pushButton_3.setGeometry(QtCore.QRect(320, 100, 115, 81))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.label = QtWidgets.QLabel(self.frame)
+        self.label.setGeometry(QtCore.QRect(180, 40, 131, 20))
+        self.label.setObjectName("label")
+        self.checkBox = QtWidgets.QCheckBox(self.frame)
+        self.checkBox.setGeometry(QtCore.QRect(460, 120, 101, 41))
+        self.checkBox.setObjectName("checkBox")
+        self.horizontalLayout.addWidget(self.frame)
+        self.verticalWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalWidget.setGeometry(QtCore.QRect(89, 319, 601, 201))
+        self.verticalWidget.setObjectName("verticalWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.grplot = PlotWidget(self.verticalWidget)
+        self.grplot.setObjectName("grplot")
+        self.verticalLayout.addWidget(self.grplot)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setObjectName("menubar")
+        self.menuRASP_Display = QtWidgets.QMenu(self.menubar)
+        self.menuRASP_Display.setObjectName("menuRASP_Display")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.menubar.addAction(self.menuRASP_Display.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Load\n"
+"Parameters"))
+        self.pushButton_2.setText(_translate("MainWindow", "Select\n"
+"Processor"))
+        self.pushButton_3.setText(_translate("MainWindow", "Run\n"
+"Process"))
+        self.label.setText(_translate("MainWindow", "Configuration Panel"))
+        self.checkBox.setText(_translate("MainWindow", "Continuous\n"
+"Update"))
+        self.menuRASP_Display.setTitle(_translate("MainWindow", "RASP Display"))
+
+from pyqtgraph import PlotWidget
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
