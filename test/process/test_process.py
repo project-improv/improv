28699c25ff21755e9e8624845413d50a5483413e
hawkwings <hawkwings@users.noreply.github.com>
2019-06-05 16:09:34 -0400

    Merge

--- test/process/test_process.py
+++ test/process/test_process.py
@@ -11,14 +11,15 @@ class Caiman_Setup(StoreDependentTestCase):
     def setUp(self):
         super(Caiman_Setup, self).setUp()
         self.limbo = Limbo()
-        self.proc = cp('caiman', self.limbo)
+        self.proc = cp()
+        self.proc.setStore(self.limbo)
 
     def test_StartProc(self):
         #cp.setupProcess(self.proc, 'params_dict')
         self.assertTrue(1)
     
     def test_runProc(self):
-        self.proc.setupProcess()
+        self.proc.setup()
         fnames = self.limbo.get('params_dict')['fnames']
         print('Test runProc: processing files: ',fnames)
         t = time.time()
