9009fe68428218726bca4e4b8334416211587144
Anne Draelos <hawkwings@users.noreply.github.com>
2019-08-02 15:09:18 -0400

    Removing old functions, updating procFrame

--- src/process/process.py
+++ src/process/process.py
@@ -40,87 +40,7 @@ class CaimanProcessor(Processor):
     '''
     def __init__(self, *args, config_file=None):
         super().__init__(*args)
-        self.ests = None #neural activity
-        self.coords = None
         self.param_file = config_file
-
-    def loadParams(self, param_file=None):
-        ''' Load parameters from file or 'defaults' into store
-            TODO: accept user input from GUI
-            This also effectively registers specific params
-            that CaimanProcessor needs with Nexus
-        '''
-        #TODO: Convert this to Tweak and load from there
-
-        if param_file is not None:
-            try:
-                params_dict = self._load_params_from_file(param_file)
-            except Exception as e:
-                logger.exception('File cannot be loaded. {0}'.format(e))
-        else:
-            # defaults from demo scripts; CNMFParams does not set
-            # each parameter needed by default (TODO change that?)
-            # TODO add parameter validation inside Tweak
-            home = expanduser("~")
-            cwd = os.getcwd()
-            params_dict = {'fnames': [cwd+'/data/zf1.h5'], #Tolias_mesoscope_2.hdf5'],
-                   'fr': 3.6,
-                   'decay_time': 0.5,
-                   'gSig': (3,3),
-                   'p': 1,
-                   'min_SNR': 1,
-                   'rval_thr': 0.9,
-                   'ds_factor': 1,
-                   'nb': 2,
-                   'motion_correct': True,
-                   'init_batch': 100,
-                   'init_method': 'bare',
-                   'normalize': True,
-                   'sniper_mode': True,
-                   'K': 2,
-                   'epochs': 1,
-                   'max_shifts_online': np.ceil(10).astype('int'),
-                   'pw_rigid': False,
-                   'dist_shape_update': True,
-                   'show_movie': False,
-                   'update_freq': 500,
-                   'minibatch_shape': 100,
-                   'output': 'outputEstimates'}
-        self.client.put(params_dict, 'params_dict')
-        #TODO: return code    
-
-    def _load_params_from_file(self, param_file):
-        '''Filehandler for loading caiman parameters
-            TODO
-        '''
-        return {}
-
-    def setup(self):
-        ''' Create OnACID object and initialize it
-                (runs initialize online)
-            limboClient is a client to the data store server
-        '''
-        logger.info('Running setup for '+self.name)
-        self.done = False
-        self.dropped_frames = []
-        self.coords = None
-        self.ests = None
-        self.A = None
-
-        self.loadParams(param_file=self.param_file)
-        self.params = self.client.get('params_dict')
-        
-        # MUST include inital set of frames
-        # TODO: Institute check here as requirement to Nexus
-        
-        self.opts = CNMFParams(params_dict=self.params)
-        self.onAc = OnACID(params = self.opts)
-        self.frame_number = 0 #self.params['init_batch']
-        #TODO: Need to rewrite init online as well to receive individual frames.
-        self.onAc.initialize_online()
-        self.max_shifts_online = self.onAc.params.get('online', 'max_shifts_online')
-
-        return self
 
     def run(self):
         '''Run the processor continually on input frames
@@ -158,11 +78,36 @@ class CaimanProcessor(Processor):
                 S = np.stack([osi.s for osi in self.onAc.estimates.OASISinstances])
                 np.savetxt('end_spikes.txt', S)
             except Exception as e:
-                #logger.error('Exception {}: {} during frame number {}'.format(type(e).__name__, e, self.frame_number))
+                logger.error('Exception {}: {} during frame number {}'.format(type(e).__name__, e, self.frame_number))
                 print(traceback.format_exc())
         else:
             print('No OASIS')
         
+
+    def setup(self):
+        ''' Create OnACID object and initialize it
+                (runs initialize online)
+        '''
+        logger.info('Running setup for '+self.name)
+        self.done = False
+        self.dropped_frames = []
+        self.coords = None
+        self.ests = None
+        self.A = None
+
+        self.loadParams(param_file=self.param_file)
+        self.params = self.client.get('params_dict')
+        
+        # MUST include inital set of frames
+        # TODO: Institute check here as requirement to Nexus
+        
+        self.opts = CNMFParams(params_dict=self.params)
+        self.onAc = OnACID(params = self.opts)
+        self.frame_number = 0 #self.params['init_batch']
+        #TODO: Need to rewrite init online as well to receive individual frames.
+        self.onAc.initialize_online()
+        self.max_shifts_online = self.onAc.params.get('online', 'max_shifts_online')
+
 
     def runProcess(self):
         ''' Run process. Runs once per frame.
@@ -210,25 +155,54 @@ class CaimanProcessor(Processor):
             # self.q_comm.put(None)
             # self.done = True
 
-    def finalProcess(self, output):    
-        if self.onAc.params.get('online', 'normalize'):
-            # normalize final estimates for this set of files. Useful?
-            #self._normAnalysis()
-            self.putEstimates(self.onAc.estimates, output)
-            #self._finalAnalysis(frame_number)
-            #self.putEstimates(self.finalEstimates, output)
-        
-        #once runProcess has run once, need to reset init_batch to avoid internal errors
-        #currently not logging this change; keeping all internal to caiman
-        self.onAc.params.set('online', {'init_batch': 0})
-        self.params['init_batch'] = 0
-        # also need to extend dimensions of timesteps...TODO: better method init?
-            #currently changed T1 inside online_cnmf....
+    def loadParams(self, param_file=None):
+        ''' Load parameters from file or 'defaults' into store
+            TODO: accept user input from GUI
+            This also effectively registers specific params
+            that CaimanProcessor needs with Nexus
+        '''
 
-        #TODO: determine a SINGLE location for params. Internal vs logged?
-        #self.client.replace(self.params, 'params_dict')
-        logger.info('Updated init batch after first run')
+        if param_file is not None:
+            try:
+                params_dict = self._load_params_from_file(param_file)
+            except Exception as e:
+                logger.exception('File cannot be loaded. {0}'.format(e))
+        else:
+            # defaults from demo scripts; CNMFParams does not set
+            # each parameter needed by default (TODO change that?)
+            # TODO add parameter validation inside Tweak
+            home = expanduser("~")
+            cwd = os.getcwd()
+            params_dict = {'fnames': [cwd+'/data/zf1.h5'], #Tolias_mesoscope_2.hdf5'],
+                   'fr': 3.6,
+                   'decay_time': 0.5,
+                   'gSig': (3,3),
+                   'p': 1,
+                   'min_SNR': 1,
+                   'rval_thr': 0.9,
+                   'ds_factor': 1,
+                   'nb': 2,
+                   'motion_correct': True,
+                   'init_batch': 100,
+                   'init_method': 'bare',
+                   'normalize': True,
+                   'sniper_mode': True,
+                   'K': 2,
+                   'epochs': 1,
+                   'max_shifts_online': np.ceil(10).astype('int'),
+                   'pw_rigid': False,
+                   'dist_shape_update': True,
+                   'show_movie': False,
+                   'update_freq': 500,
+                   'minibatch_shape': 100,
+                   'output': 'outputEstimates'}
+        self.client.put(params_dict, 'params_dict')
 
+    def _load_params_from_file(self, param_file):
+        '''Filehandler for loading caiman parameters
+            TODO
+        '''
+        return {}
 
     def putEstimates(self, estimates, output):
         ''' Put whatever estimates we currently have
@@ -248,17 +222,11 @@ class CaimanProcessor(Processor):
                     print('osi.s shape ', self.onAc.estimates.OASISinstances[0].s.shape)
                     if before < self.dropped_frames[0]:
                         tmp = np.stack([osi.s[before:self.dropped_frames[0]] for osi in self.onAc.estimates.OASISinstances])
-                        #print('before', before, 'dropped_frame[0]', self.dropped_frames[0])
-                        #print('tmp.shape', tmp.shape)
                         S[:tmp.shape[0],:self.dropped_frames[0]-before] = tmp
                     good_frames = np.array([f for f in range(before,self.frame_number,1) if f not in self.dropped_frames])
                     if good_frames.shape[0] > 0:
                         if good_frames[0] < self.onAc.estimates.OASISinstances[0].s.shape[0]: #may not have oasis for these frames yet
                             tmp2 = np.stack([osi.s[good_frames] for osi in self.onAc.estimates.OASISinstances])
-                        #print('good_frames.shape', good_frames.shape)
-                        #print('good frames min max', good_frames[0], good_frames[1])
-                        #print('tmp2 shape', tmp2.shape)
-                        #print('before, frame number', before, self.frame_number)
                             S[:tmp2.shape[0], good_frames-before] = tmp2
                         else: #TODO just for testing
                             tmp2 = np.stack([osi.s[-good_frames.shape[0]:] for osi in self.onAc.estimates.OASISinstances])
@@ -296,81 +264,16 @@ class CaimanProcessor(Processor):
 
         self.putAnalysis_time.append([time.time()-t, t2-t, t3-t2, t4-t3, t5-t4, t6-t5])
     
-    def _updateCoords(self, A, dims):
-        '''See if we need to recalculate the coords
-           Also see if we need to add components
-        '''
-        if self.coords is None: #initial calculation
-            self.A = A
-            self.coords = get_contours(A, dims)
-
-        elif np.shape(A)[1] > np.shape(self.A)[1] and self.frame_number % 10 == 0: 
-            #Only recalc if we have new components
-            # FIXME: Since this is only for viz, only do this every 100 frames
-            # TODO: maybe only recalc coords that are new? 
-            #print('Recalc coords ', self.counter)
-            self.A = A
-            self.coords = get_contours(A, dims)
-            self.counter += 1
-
-    def makeImage(self):
-        '''Create image data for visualiation
-            Using caiman code here
-            #TODO: move to MeanAnalysis class ?? Check timing if we move it!
-                Other idea -- easier way to compute this?
-        '''
-        mn = self.onAc.M - self.onAc.N
-        image = None
-        try:
-            components = self.onAc.estimates.Ab[:,mn:].dot(self.onAc.estimates.C_on[mn:self.onAc.M,(self.frame_number-1)%self.onAc.window]).reshape(self.onAc.dims, order='F')
-            background = self.onAc.estimates.Ab[:,:mn].dot(self.onAc.estimates.C_on[:mn,(self.frame_number-1)%self.onAc.window]).reshape(self.onAc.dims, order='F')
-            image = ((components + background) - self.onAc.bnd_Y[0])/np.diff(self.onAc.bnd_Y)
-            image = np.minimum((image*255.),255).astype('u1')
-        except ValueError as ve:
-            logger.info('ValueError: {0}'.format(ve))
-
-        #cor_frame = (self.frame - self.onAc.bnd_Y[0])/np.diff(self.onAc.bnd_Y)
-
-        # print('dtype raw frame:', self.frame.dtype)
-        # print('dtype cor_frame:', cor_frame.dtype)
-
-        return image
-
-    def _finalAnalysis(self, t):
-        ''' Some kind of final steps for estimates
-            t is the final frame count
-            TODO: get params from elsewhere in case changed!
-            Can't put these into onAc.estimates since it changes shape,
-            and thus interferes with running the process again in a loop.
-        '''
-        #TODO change epochs?
-        epochs = 1
-
-        self.finalEstimates = self.onAc.estimates
-        
-        self.finalEstimates.A, self.finalEstimates.b = self.onAc.estimates.Ab[:, self.onAc.params.get('init', 'nb'):], self.onAc.estimates.Ab[:, :self.onAc.params.get('init', 'nb')].toarray()
-        self.finalEstimates.C, self.finalEstimates.f = self.onAc.estimates.C_on[self.onAc.params.get('init', 'nb'):self.onAc.M, t - t //
-                         epochs:t], self.onAc.estimates.C_on[:self.onAc.params.get('init', 'nb'), t - t // epochs:t]
-        noisyC = self.onAc.estimates.noisyC[self.onAc.params.get('init', 'nb'):self.onAc.M, t - t // epochs:t]
-        self.finalEstimates.YrA = noisyC - self.onAc.estimates.C
-        self.finalEstimates.bl = [osi.b for osi in self.onAc.estimates.OASISinstances] if hasattr(self, 'OASISinstances') else [0] * self.onAc.estimates.C.shape[0]
-        self.finalEstimates.C_on = self.onAc.estimates.C_on[:self.onAc.M]
-        self.finalEstimates.noisyC = self.onAc.estimates.noisyC[:self.onAc.M]
-
 
     def _checkFrames(self):
         ''' Check to see if we have frames for processing
-            TODO: rework logic since not accessing store directly here anymore
         '''
         try:
             res = self.q_in.get(timeout=0.0001)
             return res
-            #return self.client.get('frame')
-        # except CannotGetObjectError:
-        #     logger.error('No frames')
         #TODO: add'l error handling
         except Empty:
-            #logger.info('no frames for processing')
+            logger.info('No frames for processing')
             return None
 
 
@@ -400,9 +303,11 @@ class CaimanProcessor(Processor):
                 raise Exception
             
             if self.onAc.params.get('motion', 'pw_rigid'):
-                frame_cor1, shift = motion_correct_iteration_fast(
-                            frame, templ, self.max_shifts_online, self.max_shifts_online)
-                frame_cor, shift = tile_and_correct(frame, templ, self.onAc.params.motion['strides'], self.onAc.params.motion['overlaps'], self.onAc.params.motion['max_shifts'], newoverlaps=None, newstrides=None, upsample_factor_grid=4, upsample_factor_fft=10, show_movie=False, max_deviation_rigid=self.onAc.params.motion['max_deviation_rigid'],add_to_movie=0, shifts_opencv=True, gSig_filt=None, use_cuda=False, border_nan='copy')[:2]
+                frame_cor, shift, _, xy_grid = tile_and_correct(frame, templ, self.onAc.params.motion['strides'], self.onAc.params.motion['overlaps'],
+                                                                            self.onAc.params.motion['max_shifts'], newoverlaps=None, newstrides=None, upsample_factor_grid=4,
+                                                                            upsample_factor_fft=10, show_movie=False, max_deviation_rigid=self.onAc.params.motion['max_deviation_rigid'],
+                                                                            add_to_movie=0, shifts_opencv=True, gSig_filt=None,
+                                                                            use_cuda=False, border_nan='copy')
             else:
                 frame_cor, shift = motion_correct_iteration_fast(frame, templ, self.max_shifts_online, self.max_shifts_online)
             self.onAc.estimates.shifts.append(shift)
@@ -413,6 +318,7 @@ class CaimanProcessor(Processor):
         self.procFrame_time.append([time.time()-t])
         return frame_cor
 
+
     def _fitFrame(self, frame_number, frame):
         ''' Do the heavy lifting here. CNMF, etc
             Updates self.onAc.estimates
@@ -420,18 +326,46 @@ class CaimanProcessor(Processor):
         try:
             self.onAc.fit_next(frame_number, frame)
         except Exception as e:
-            print('Fit frame Error!! {}: {}'.format(type(e).__name__, e))
+            logger.error('Fit frame error! {}: {}'.format(type(e).__name__, e))
             raise Exception
 
-    def _normAnalysis(self):
-        ''' Modifies in place Ab!
-        Do not use until the end, or
-        TODO: just return nicely normalized Ab. Possibly combine with other
-            visualization/estimation output
-        '''
-        self.onAc.estimates.Ab /= 1./self.onAc.img_norm.reshape(-1, order='F')[:,np.newaxis]
-        self.onAc.estimates.Ab = scipy.sparse.csc_matrix(self.onAc.estimates.Ab)
 
+    def _updateCoords(self, A, dims):
+        '''See if we need to recalculate the coords
+           Also see if we need to add components
+        '''
+        if self.coords is None: #initial calculation
+            self.A = A
+            self.coords = get_contours(A, dims)
+
+        elif np.shape(A)[1] > np.shape(self.A)[1] and self.frame_number % 10 == 0: 
+            #Only recalc if we have new components
+            # FIXME: Since this is only for viz, only do this every 100 frames
+            # TODO: maybe only recalc coords that are new? 
+            self.A = A
+            self.coords = get_contours(A, dims)
+            self.counter += 1
+
+
+    def makeImage(self):
+        '''Create image data for visualiation
+            Using caiman code here
+            #TODO: move to MeanAnalysis class ?? Check timing if we move it!
+                Other idea -- easier way to compute this?
+        '''
+        mn = self.onAc.M - self.onAc.N
+        image = None
+        try:
+            components = self.onAc.estimates.Ab[:,mn:].dot(self.onAc.estimates.C_on[mn:self.onAc.M,(self.frame_number-1)%self.onAc.window]).reshape(self.onAc.dims, order='F')
+            background = self.onAc.estimates.Ab[:,:mn].dot(self.onAc.estimates.C_on[:mn,(self.frame_number-1)%self.onAc.window]).reshape(self.onAc.dims, order='F')
+            image = ((components + background) - self.onAc.bnd_Y[0])/np.diff(self.onAc.bnd_Y)
+            image = np.minimum((image*255.),255).astype('u1')
+        except ValueError as ve:
+            logger.info('ValueError: {0}'.format(ve))
+
+        #cor_frame = (self.frame - self.onAc.bnd_Y[0])/np.diff(self.onAc.bnd_Y)
+
+        return image
 
 class NaNFrameException(Exception):
     pass
