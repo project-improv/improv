bfdfe174d8b9f33eb7280b68d37450b880f0ac40
Anne Draelos <hawkwings@users.noreply.github.com>
2019-10-11 10:27:36 -0400

    Integrating Max's zmq formating for frame and stim acq

--- src/acquire/acquirer_zmq.py
+++ src/acquire/acquirer_zmq.py
@@ -5,6 +5,7 @@ import time
 import h5py
 import numpy as np
 import zmq
+import json
 
 from nexus.actor import Actor, RunManager
 
@@ -26,13 +27,17 @@ class ZMQAcquirer(Actor):
         if not 0 <= port <= 65535:
             raise ValueError(f'Port {self.port} invalid.')
 
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.SUB)
-        self.socket.connect(f"tcp://{self.ip}:{self.port}")
-        self.socket.setsockopt_string(zmq.SUBSCRIBE, self.topicfilter)
+        # self.context = zmq.Context()
+        # self.socket = self.context.socket(zmq.SUB)
+        # self.socket.connect(f"tcp://{self.ip}:{self.port}")
+        # self.socket.setsockopt_string(zmq.SUBSCRIBE, self.topicfilter)
 
     def setup(self):
-        pass
+        context = zmq.Context()
+        self.socket = context.socket(zmq.SUB)
+        self.socket.connect("tcp://10.122.170.21:4701")
+        self.socket.connect("tcp://10.122.170.21:4702")
+        self.socket.setsockopt(zmq.SUBSCRIBE, b'')
 
     def run(self):
         '''Triggered at Run
@@ -53,14 +58,38 @@ class ZMQAcquirer(Actor):
         ''' Main loop. If there're new files, read and put into store.
         '''
         t = time.time()
+        #TODO: use poller instead to prevent blocking, include a timeout
+        try:
+            msg = self.socket.recv(flags=zmq.NOBLOCK)
+            msg_parts = [part.strip() for part in msg.split(b': ', 1)]
+            tag = msg_parts[0].split(b' ')[0]
 
-        data = self.socket.recv_string()
-        self.check_input(data)
-        obj_id = self.client.put(data, 'acq_raw' + str(self.frame_num))
-        self.q_out.put([{str(self.frame_num): obj_id}])
+            if tag == b'stimid':
+                print('stimulus id: {}'.format(msg_parts[1]))
+                # output example: stimulus id: b'background_stim'
 
-        self.frame_num += 1
-        self.total_times.append(time.time() - t)
+                self.links['stim_queue'].put({self.frame_num:msg_parts[1]}) #TODO: stimID needs to be numbered?
 
-    def check_input(self, data):
-        pass
+            elif tag == b'frame':
+                t0 = time.time()
+                array = np.array(json.loads(msg_parts[1]))  # assuming the following message structure: 'tag: message'
+                print('{} messsage length: {}. Element sum: {}; time to process: {}'.format(msg_parts[0], len(msg),
+                                                                                            array.sum(), time.time() - t0))
+                # output example: b'frame ch0 10:02:01.115 AM 10/11/2019' messsage length: 1049637. Element sum: 48891125; time to process: 0.04192757606506348
+                
+                obj_id = self.client.put(array, 'acq_raw' + str(self.frame_num))
+                self.q_out.put([{str(self.frame_num): obj_id}])
+
+                self.frame_num += 1
+                self.total_times.append(time.time() - t)
+
+            else:
+                if len(msg) < 100:
+                    print(msg)
+                else:
+                    print('msg length: {}'.format(len(msg)))
+
+        except zmq.Again as e:
+            pass #no messages available
+        except Exception as e:
+            print('error: {}'.format(e))
