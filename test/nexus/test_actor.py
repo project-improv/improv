c274790ad183fa93a40bf4e797b4e7a68e3ab456
NICOLE MOISEYEV <nicole.moiseyev3400@gmail.com>
2019-10-28 13:33:55 -0400

    Write actor setStore unittest

--- test/nexus/test_actor.py
+++ test/nexus/test_actor.py
@@ -2,6 +2,7 @@
 from unittest import TestCase
 from test.test_utils import StoreDependentTestCase
 from src.nexus.actor import Actor, RunManager, AsyncRunManager, Spike
+from src.nexus.store import Limbo
 from multiprocessing import Process
 from pyarrow import PlasmaObjectExists
 from scipy.sparse import csc_matrix
@@ -10,3 +11,41 @@ import pyarrow.plasma as plasma
 from pyarrow.lib import ArrowIOError
 from nexus.store import ObjectNotFoundError
 import pickle
+
+
+#TODO: write actor unittests
+
+#setStore
+class Actor_setStore(StoreDependentTestCase):
+    def setUp(self):
+        super(Actor_setStore, self).setUp()
+        self.actor = Actor('test')
+        self.limbo = Limbo()
+
+    def test_setStore(self):
+        limbo = self.limbo
+        actor = self.actor
+        actor.setStore(limbo.client)
+        self.assertEqual(actor.client, limbo.client)
+
+    def tearDown(self):
+        super(Actor_setStore, self).tearDown()
+
+
+#setLinks
+
+#setCommLinks
+
+#setLinkIn
+
+#setLinkOut
+
+#addLink
+
+#getLinks
+
+#setUp
+
+#run
+
+#changePriority
