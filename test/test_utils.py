eec62fc5266fe29dd821fcd0dc3917ac8a3fa9c5
Daniel Sprague <dys9@duke.edu>
2019-10-30 14:39:51 -0400

    fixed runManager

--- test/test_utils.py
+++ test/test_utils.py
@@ -20,4 +20,26 @@ class StoreDependentTestCase(TestCase):
         '''
 
         self.p.kill()
-        
+    
+class ActorDependentTestCase(TestCase):
+
+    def setUp(self):
+        ''' Start the server
+        '''
+        self.p = subprocess.Popen(['plasma_store',
+                              '-s', '/tmp/store',
+                              '-m', str(10000000)],
+                              stdout=subprocess.DEVNULL,
+                              stderr=subprocess.DEVNULL)
+
+    def run_setup(self):
+        self.isSetUp= True
+
+    def runMethod(self):
+        self.runNum+=1
+
+    def tearDown(self):
+        ''' Kill the server
+        '''
+
+        self.p.kill()
