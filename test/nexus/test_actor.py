e60751dbac6a7d201e2b8a794aaf4ae05abc1b1f
NICOLE MOISEYEV <nicole.moiseyev3400@gmail.com>
2019-11-04 12:46:34 -0500

    Fix merge conflicts

--- test/nexus/test_actor.py
+++ test/nexus/test_actor.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 from test.test_utils import StoreDependentTestCase, ActorDependentTestCase
 from src.nexus.actor import Actor, RunManager, AsyncRunManager, Spike
 from src.nexus.store import Limbo
+from src.nexus.nexus import AsyncQueue
 from multiprocessing import Process
 import numpy as np
 from pyarrow.lib import ArrowIOError
@@ -67,7 +68,7 @@ class RunManager_setupRun(ActorDependentTestCase):
         q_sig= Queue()
         q_sig.put('setup')
         q_sig.put('run') #runs after this signal
-        q_sig.put('pause') 
+        q_sig.put('pause')
         q_sig.put('resume') #runs again after this signal
         q_sig.put('quit')
         q_comm= Queue()
@@ -79,7 +80,33 @@ class RunManager_setupRun(ActorDependentTestCase):
     def tearDown(self):
         super(RunManager_setupRun, self).tearDown()
 
+class AsyncRunManager_setupRun(ActorDependentTestCase):
 
+    def setUp(self):
+        super(AsyncRunManager_setupRun, self).setUp()
+        self.actor=Actor('test')
+        self.isSetUp= False;
+        q_sig = Queue()
+        self.q_sig = AsyncQueue(q_sig,'test_sig','test_start', 'test_end')
+        q_comm = Queue()
+        self.q_comm = AsyncQueue(q_comm, 'test_comm','test_start', 'test_end')
+        self.runNum=0
+
+    def load_queue(self):
+        self.a_put("setup", 0.1)
+        self.a_put('setup',0.1)
+        self.a_put('run',0.1)
+        self.a_put('pause',0.1)
+        self.a_put('resume',0.1)
+        self.a_put('quit',0.1)
+
+    async def test_asyncRunManager(self):
+        await AsyncRunManager('test', self.runMethod, self.run_setup, self.q_sig, self.q_comm)
+        self.assertEqual(self.runNum, 2)
+        self.assertTrue(self.isSetUp)
+
+    def tearDown(self):
+        super(AsyncRunManager_setupRun, self).tearDown()
 
 class Actor_setLinks(ActorDependentTestCase):
 
@@ -128,4 +155,3 @@ class Actor_setLinkIn(ActorDependentTestCase):
 
     def tearDown(self):
         super(Actor_setLinkIn, self).tearDown()
-
