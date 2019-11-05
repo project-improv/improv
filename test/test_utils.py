9e0cef2998076499eace46aedec3951996700915
Daniel Sprague <dys9@duke.edu>
2019-11-05 16:10:28 -0500

    add new process

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 import subprocess
 import asyncio
+from nexus.actor import RunManager
 
 class StoreDependentTestCase(TestCase):
     ''' Unit test base class that starts the Limbo plasma server
@@ -39,10 +40,20 @@ class ActorDependentTestCase(TestCase):
     def runMethod(self):
         self.runNum+=1
 
+    def process_setup(self):
+        self.q_comm.put(True)
+
+    def process_run(self):
+        self.q_comm.put('ran')
+
+    def createprocess(self, q_sig, q_comm):
+        with RunManager('test', self.runMethod, self.process_setup, q_sig, q_comm) as rm:
+            print(rm)
+        
     async def a_put(self, signal, time):
         await asyncio.sleep(time)
-        self.q_sig.put_async(signal) 
-        
+        self.q_sig.put_async(signal)      
+
 
     def tearDown(self):
         ''' Kill the server
