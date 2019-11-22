93dac921affc1f112fef84dd64b1e4576c4330c0
Daniel Sprague <dys9@duke.edu>
2019-11-22 11:22:20 -0500

    adf

--- test/test_utils.py
+++ test/test_utils.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 import subprocess
 import asyncio
 from nexus.actor import RunManager, AsyncRunManager
+from multiprocessing import Process
 
 class StoreDependentTestCase(TestCase):
     ''' Unit test base class that starts the Limbo plasma server
@@ -55,11 +56,33 @@ class ActorDependentTestCase(TestCase):
     def createprocess(self, q_sig, q_comm):
         with RunManager('test', self.process_run, self.process_setup, q_sig, q_comm) as rm:
             print(rm)
+
+    async def count(self):
+        self.runlist.append(1) 
+        await asyncio.sleep(1)
+        self.runlist.append(2)
+
+    async def main(self):
+        await asyncio.gather(self.count(), self.count(), self.count())
+
+    def process(self):
+        asyncio.run(self.main())
     
     async def createAsyncProcess(self, q_sig, q_comm):
         async with AsyncRunManager('test', self.process_run, self.process_setup, q_sig, q_comm) as rm:
             print(rm)
-        
+
+    async def runProcess(self, q_sig, q_comm):
+        self.p2 = Process(target= self.createAsyncProcess, args= (self.q_sig, self.q_comm,))
+        self.p2.start()
+        self.q_sig.put('setup')
+        self.q_sig.put('run')
+        self.q_sig.put('pause')
+        self.q_sig.put('resume')
+        self.q_sig.put('quit')
+        self.p2.terminate()
+        self.p2.kill()
+
     async def a_put(self, signal, time):
         await asyncio.sleep(time)
         self.q_sig.put_async(signal)      
