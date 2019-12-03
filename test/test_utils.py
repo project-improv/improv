e65cef571a8326334274238a78ab9263354109dc
Daniel Sprague <dys9@duke.edu>
2019-12-03 13:02:57 -0500

    A to data

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 import subprocess
 import asyncio
+from nexus.actor import RunManager, AsyncRunManager
+from multiprocessing import Process
 
 class StoreDependentTestCase(TestCase):
     ''' Unit test base class that starts the Limbo plasma server
@@ -21,7 +23,7 @@ class StoreDependentTestCase(TestCase):
         '''
 
         self.p.kill()
-   
+
 class ActorDependentTestCase(TestCase):
 
     def setUp(self):
@@ -33,19 +35,32 @@ class ActorDependentTestCase(TestCase):
                               stdout=subprocess.DEVNULL,
                               stderr=subprocess.DEVNULL)
 
+    def tearDown(self):
+        ''' Kill the server
+        '''
+
+        self.p.kill()
+
     def run_setup(self):
         self.isSetUp= True
 
     def runMethod(self):
         self.runNum+=1
 
+    def process_setup(self):
+        pass
+
+    def process_run(self):
+        self.q_comm.put('ran')
+
+    def createprocess(self, q_sig, q_comm):
+        with RunManager('test', self.process_run, self.process_setup, q_sig, q_comm) as rm:
+            print(rm)
+
+    async def createAsyncProcess(self, q_sig, q_comm):
+        async with AsyncRunManager('test', self.process_run, self.process_setup, q_sig, q_comm) as rm:
+            print(rm)
+
     async def a_put(self, signal, time):
         await asyncio.sleep(time)
-        self.q_sig.put_async(signal) 
-        
-
-    def tearDown(self):
-        ''' Kill the server
-        '''
-
-        self.p.kill()
+        self.q_sig.put_async(signal)
