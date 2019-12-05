d4c3d460464dd0c60937af62aad452cd69384940
NICOLE MOISEYEV <nicole.moiseyev3400@gmail.com>
2019-12-05 16:11:28 -0500

    Plot results of csc/normal matrix put/get timings

--- src/csc_timings.py
+++ src/csc_timings.py
@@ -2,6 +2,8 @@ import timeit
 import os
 import sys
 sys.path.append(".")
+import matplotlib.pyplot as plt
+
 
 from unittest import TestCase
 from nexus.store import Limbo
@@ -21,7 +23,7 @@ def time_putget(n, type = "normal"):
     # Start the process
     p = subprocess.Popen(['plasma_store',
                       '-s', '/tmp/store',
-                      '-m', str(1E10)],
+                      '-m', str(10000000000)],
                       stdout=subprocess.DEVNULL,
                       stderr=subprocess.DEVNULL)
     limbo = Limbo()
@@ -37,29 +39,78 @@ def time_putget(n, type = "normal"):
     # Print object size
     #size = sys.getsizeof(matrix)
     #print(size)
-    print(matrix.data.nbytes)
+    size = matrix.data.nbytes
+    print("Size: ", size)
 
     # Time putting and getting to store
     print("Putting {type} matrix of size {n} by {n}".format(type = type, n = n))
     t = timeit.default_timer()
     limbo.put(matrix, "matrix")
-    print('\tTime', timeit.default_timer() - t)
+    putTime = timeit.default_timer() - t
+    print('\tTime', putTime)
+
+
 
     print("Getting {type} matrix of size {n} by {n}".format(type = type, n = n))
     t = timeit.default_timer()
     limbo.get("matrix")
-    print('\tTime', timeit.default_timer() - t)
+    getTime = timeit.default_timer() - t
+    print('\tTime', getTime)
 
     # Kill the process
     p.kill()
 
+    # Return timings
 
+    return putTime, getTime, size
+
+
+
+timings = {
+    "csc": {
+        "put": [],
+        "get": [],
+        "size": []
+    },
+    "normal": {
+        "put": [],
+        "get": [],
+        "size": []
+    }
+}
+
+
+def updateTimings(putTime, getTime, size, type = "normal"):
+    timings[type]["put"].append(putTime)
+    timings[type]["get"].append(getTime)
+    timings[type]["size"].append(size)
 
 # TODO: add timings up to 1gb
 # Run results
-for i in range(8000,8005):
-    time_putget(i)
-    time_putget(i, type = "csc")
+for i in range(100,500):
+    putTime, getTime, size = time_putget(i)
+    updateTimings(putTime, getTime, size)
+
+    putTime, getTime, size = time_putget(i, type = "csc")
+    updateTimings(putTime, getTime, size, type = "csc")
 
 
+print(timings)
 # TODO: Plot results
+
+
+def  plotTimings(type):
+    putTimes = timings[type]["put"]
+    getTimes = timings[type]["get"]
+    sizes = timings[type]["size"]
+
+    plt.plot(sizes, putTimes, label = "Put, {}".format(type))
+    plt.plot(sizes, getTimes, label = "Get, {}".format(type))
+    plt.ylabel("Time of operation (seconds)")
+    plt.xlabel("Size of matrix (bytes)")
+
+
+plotTimings("normal")
+plotTimings("csc")
+plt.legend()
+plt.show()
