0cae062b2efc1cc19f8cfbbb0024a4ffe1a0325a
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2020-01-19 16:34:29 -0500

    Update entrypoint to Docker

--- docker/suite2p/entrypoint_suite2p.sh
+++ docker/suite2p/entrypoint_suite2p.sh
@@ -2,7 +2,7 @@
 
 source activate improv-suite
 cd /opt
-git clone --single-branch --branch suite2p https://github.com/pearsonlab/improv
+git clone --single-branch --branch docker https://github.com/pearsonlab/improv
 cd improv
 plasma_store -s /tmp/store -m 3000000000 &>/dev/null &
 python -m nexus.nexus
