b0d2e798ab67add55f8dadebbc022cbf3b1d7d86
Anne Draelos <hawkwings@users.noreply.github.com>
2020-07-10 15:55:49 -0400

    Folder acquirer for tiff files in neurofinder demo

---
+++ demos/neurofinder/actors/acquire_folder.py
@@ -0,0 +1,94 @@
+import time
+import os
+import h5py
+import struct
+import numpy as np
+import random
+
+from pathlib import Path
+from skimage.io import imread
+from improv.actor import Actor, Spike, RunManager
+from queue import Empty
+
+import logging; logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+class FolderAcquirer(Actor):
+    ''' Current behavior is looping over all files in a folder.
+        Class to read TIFF files in a specified {path} from disk.
+        Designed for scenarios when new TIFF files are created during the run.
+        Reads only new TIFF files (after Run started) and put on to the Plasma store.
+        If there're multiple files, files are loaded by name.
+    '''
+
+    def __init__(self, *args, folder=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.data = None
+        self.done = False
+        self.flag = False
+        self.path = Path(folder)
+
+        self.frame_num = 0
+        self.files = set()
+
+        if not self.path.exists() or not self.path.is_dir():
+            raise AttributeError('Folder {} does not exist.'.format(self.path))
+
+    def setup(self):
+        pass
+        
+    def saveImgs(self):
+        self.imgs = []
+        files = {f for f in self.path.iterdir() if f.suffix in ['.tif', '.tiff']}
+        files = sorted(list(files))
+        for file in files:
+            img = self.get_tiff(file)
+            self.imgs.append(img)
+        self.imgs = np.array(self.imgs)
+        f = h5py.File('output/sample.h5', 'w', libver='latest')
+        f.create_dataset("default", data=self.imgs)
+        f.close()
+
+    def run(self):
+        ''' Triggered at Run
+            Get list of files in the folder and use that as the baseline.
+        '''
+        self.total_times = []
+        self.timestamp = []
+
+        self.files = {f for f in self.path.iterdir() if f.suffix in ['.tif', '.tiff']}
+
+        with RunManager(self.name, self.runAcquirer, self.setup, self.q_sig, self.q_comm) as rm:
+            print(rm)
+
+        print('Acquire broke, avg time per frame: ', np.mean(self.total_times))
+        print('Acquire got through ', self.frame_num, ' frames')
+
+    def runAcquirer(self):
+        ''' Main loop. If there're new files, read and put into store.
+        '''
+        t = time.time()
+        files_current = {f for f in self.path.iterdir() if f.suffix in ['.tif', '.tiff']}
+        files_new = files_current - self.files
+        # files_new = files_current  # Remove before use.
+
+        if len(files_new) == 0:
+            time.sleep(0.01) # Remove before use
+
+        else:  # New files
+            files_new = sorted(list(files_new))
+            for file in files_new:
+                obj_id = self.client.put(self.get_tiff(file), 'acq_raw' + str(self.frame_num))
+                self.q_out.put([{str(self.frame_num): obj_id}])
+                self.frame_num += 1
+                self.files.add(file)
+
+            self.total_times.append(time.time() - t)
+
+    def get_tiff(self, file: Path):
+        try:
+            img = imread(file.as_posix())
+        except ValueError as e:
+            img = imread(file.as_posix())
+            logger.error('File '+file.as_posix()+' had value error {}'.format(e))
+        return img #[0,0,0, :, :,0]  #Extract first channel in this image set.
