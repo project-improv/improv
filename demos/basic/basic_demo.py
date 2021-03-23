64dde444a36895f54abfbaa35c9dd88c34a5408f
saraliszeski <75336173+saraliszeski@users.noreply.github.com>
2021-03-22 22:04:58 -0400

    Update basic_demo.py

--- demos/basic/basic_demo.py
+++ demos/basic/basic_demo.py
@@ -25,7 +25,7 @@ current_directory = os.path.dirname(__file__)
 parent_directory = os.path.split(current_directory)[0] # Repeat as needed
 parent_directory = os.path.split(parent_directory)[0] # Repeat as needed
 
-file_path = os.path.join(parent_directory, '/demodata', 'Tolias_mesoscope_2.hdf5')
+file_path = os.path.join(parent_directory, '/demodata', '/Tolias_mesoscope_2.hdf5')
 file1 = open(file_path)
 
 directory = "data" 
