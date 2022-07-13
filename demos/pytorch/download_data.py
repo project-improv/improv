a445cbb4c5c7807c6151afb7914711212cbef9c3
eaogorman <eao21@duke.edu>
2022-07-13 02:04:25 -0400

    Created demos/pytorch: actors, setup, and yaml

---
+++ demos/pytorch/download_data.py
@@ -0,0 +1,19 @@
+import torch
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+# Download training data from open datasets.
+training_data = datasets.MNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+# Download test data from open datasets.
+test_data = datasets.MNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
