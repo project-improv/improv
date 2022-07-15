4f3eef6f77929ad748f18a3ac6cc0232218a39ad
eaogorman <eao21@duke.edu>
2022-07-15 17:02:44 -0400

    Rewrote PyTorch downloads and processor

--- demos/pytorch/actors/pytorch_processor.py
+++ demos/pytorch/actors/pytorch_processor.py
@@ -10,7 +10,10 @@ from improv.actor import Actor, RunManager
 import traceback
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torchvision.models import resnet50, ResNet50_Weights
 # torchvision NOT installed due to OSError: [Errno 28] No space left on device: '/home/eao21/miniconda33/envs/improv.lib.python3.9/site-packages/torchvision-0.13.0.dist-info'
 # Mapped to location not in-memory: TMPDIR=/home/eao21/tmp pip install
 # https://github.com/pypa/pip/issues/7745
@@ -24,29 +27,75 @@ logger.setLevel(logging.INFO)
 
 class PyTorchProcessor(Actor):
     # TODO: Update ALL docstrings
+    # TODO: Clean commented sections
     # TODO: add GPU/CPU option as input...device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     ''' Using PyTorch
     '''
 
-    def __init__(self, *args, data_path=None, config_file=None):
+    def __init__(self, *args, data_path=None, model_path=None, config_file=None):
         super().__init__(*args)
-        # Add logger.info?
-        # Add print statement - data_path
-        print('data ', data_path, 'config_file', config_file)
+        logger.info(data_path, model_path)
+        # Add print statements - data_path
+        # Only for simulation, data = not acquired in real-time, no need to print...
+        # Make f-statements/strings
+        # print('Data: ', data_path)
+        # print('Model: ', model_path)
+        # print('config_file: ', config_file)
         self.param_file = config_file
-        self.data_path = data_path
+        # Not necessary for real running...only for playing around w/different data + models
+        if data_path is None:
+            logger.error('Must specify data path.')
+        else:
+            self.data_path = data_path
+        if model_path is None:
+            logger.error('Must specify pre-trained model.')
+        else:
+            self.model_path = model_path
 
-    def setup(self):
+    def load_data(self, data_path=None)
+        # Specify for data type...this is specifically for AlexNet-transformed MNIST data...
+        # Depends on file type...working w/downloaded data from PyTorch torchvision for now
+
+        
+
+        # NOTE: FOR ACTUAL MODELS TO BE INTEGRATED W/PYTORCH, WE MUST USE TORCH.JIT or something NOT DATALOADER!
+        # NOTE: Time to load data - 
+        # Images one-by-one
+        # Images in batches
+        # Create DataLoader
+        # Example to lock data so different processes don't simultaneously download the data and cause data corruption, if running multiple processes...actors...that download/load dataset
+        # from filelock import FileLock
+        # with FileLock('./data.lock'):
+        #     DataLoader(train_subset, batch_size=batch_size, shuffle=True)
+
+        self.train_loader = DataLoader(train_subset, batch_size=batch_size, shuffle=True)
+        self.test_loader = DataLoader(test_subset, batch_size=batch_size, shuffle=False)
+
+    def setup(self, model=None, weights=None):
         ''' Setup PyTorch
         '''
-                logger.info('Running setup for '+self.name)
-        self.done = False
-        # Saving model?
-        self.saving = False
+        # Really only for demo...
+        if model is not None:
+            self.model = model
+        else:
+            print('Specify model.')
+        if weights is not None:
+            self.weights = weights
+        else:
+            print('Specify weights.')
+        logger.info('Running setup for '+self.name)
 
-        self.loadParams(param_file=self.param_file)
-        self.params = self.client.get('params_dict')
+        # Init model object
+        # Initialize model
+        self.weights =
+        self.model =
+
+        # Set model to eval mode
+        model.eval()
+
+        # self.loadParams(param_file=self.param_file)
+        # self.params = self.client.get('params_dict')
 
     # def loadParams(self, param_file=None):
     #     ''' Load parameters from file or 'defaults' into store
@@ -64,8 +113,13 @@ class PyTorchProcessor(Actor):
     #     self.client.put(params_dict, 'params_dict')
 
     def run(self):
-        ''' Run the processor on input data?
+        ''' Run the processor on input data
         '''
+
+    def load_model(self, model_path=None):
+        # Initialize model
+        self.model = torch.jit.load('model_scripted.pt')
+        self.model.eval()
 
 # Create dataloaders for model...
 # TODO: input = output from download_data.py and batch_size
