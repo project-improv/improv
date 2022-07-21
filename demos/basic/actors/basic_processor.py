998ffbf18085979ab742aea8f5878e904354a910
eaogorman <eao21@duke.edu>
2022-07-21 01:47:33 -0400

    Updated demos, fixed models/acquire/processor

--- demos/basic/actors/basic_processor.py
+++ demos/basic/actors/basic_processor.py
@@ -167,7 +167,7 @@ class BasicProcessor(CaimanProcessor):
             self.counter += 1
 
 
-    def     (self):
+    def makeImage(self):
         '''Create image data for visualiation
             Using caiman code here
             #TODO: move to MeanAnalysis class ?? Check timing if we move it!
--- demos/pytorch/CIFAR10.py
+++ demos/pytorch/CIFAR10.py
@@ -7,16 +7,17 @@ from torchvision.utils import save_image
 # Run in ~/improv/demos/pytorch
 def main():
 
-    os.makedirs("./data/CIFAR10", exist_ok=True)
+    root = "/home/eao21/improv/demos/pytorch/data/"
+    os.makedirs(root + "/CIFAR10", exist_ok=True)
 
-    batch_size = 100
+    batch_size = 200
     
-    dataset = datasets.CIFAR10(root="./data", download=True, transform=ToTensor())
+    dataset = datasets.CIFAR10(root=root, download=True, transform=ToTensor())
     dataset = iter(DataLoader(dataset, batch_size=batch_size))
     
     for i in range(batch_size):
         x, _ = (next(dataset))
-        save_image(x[0], "./data/CIFAR10/{}.jpg".format(i))
+        save_image(x[0], root + "CIFAR10/{}.jpg".format(i))
 
 if __name__ == "__main__":
     main()
