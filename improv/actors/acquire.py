8ff2bde73ebd5b5c5fb077982a51311d01fdb895
eaogorman <eao21@duke.edu>
2022-07-21 18:35:48 -0400

    Added FolderAcquirer to improv/actors/acquire.py

--- improv/actors/acquire.py
+++ improv/actors/acquire.py
@@ -13,6 +13,7 @@ logger.setLevel(logging.INFO)
 # Classes: File Acquirer, Stim, Behavior, Tiff
 
 class FileAcquirer(Actor):
+    # TODO: Make file type-/extension-agnostic?
     '''Class to import data from files and output
        frames in a buffer, or discrete.
     '''
@@ -27,9 +28,9 @@ class FileAcquirer(Actor):
 
     def setup(self):
         '''Get file names from config or user input
-            Also get specified framerate, or default is 10 Hz
-           Open file stream
-           #TODO: implement more than h5 files
+        Also get specified framerate, or default is 10 Hz
+        Open file stream
+        # TODO: implement more than .h5 files
         '''
         print('Looking for ', self.filename)
         if os.path.exists(self.filename):
@@ -79,9 +80,9 @@ class FileAcquirer(Actor):
             ## simulate frame-dropping
             # if self.frame_num > 1500 and self.frame_num < 1800:
             #     frame = None
-            t= time.time()
-            id = self.client.put(frame, 'acq_raw'+str(self.frame_num))
-            t1= time.time()
+            t = time.time()
+            id = self.client.put(frame, 'acq_raw' + str(self.frame_num))
+            t1 = time.time()
             self.timestamp.append([time.time(), self.frame_num])
             try:
                 self.put([[id, str(self.frame_num)]], save=[True])
@@ -90,7 +91,7 @@ class FileAcquirer(Actor):
             except Exception as e:
                 logger.error('Acquirer general exception: {}'.format(e))
 
-            time.sleep(self.framerate) #pretend framerate
+            time.sleep(self.framerate) # pretend framerate
             self.total_times.append(time.time()-t)
 
         else: # simulating a done signal from the source (eg, camera)
@@ -112,6 +113,100 @@ class FileAcquirer(Actor):
         self.dset[self.frame_num-1] = frame
         self.f.flush()
 
+class FolderAcquirer(Actor):
+    # TODO: Make file type-/extension-agnostic?
+    ''' Current behavior is looping over all files in a folder.
+        Class to read TIFF/JPG/PNG files in a specified {path} from disk.
+        Designed for scenarios when new TIFF files are created during the run.
+        Reads only new TIFF files (after Run started) and put on to the Plasma store.
+        If there're multiple files, files are loaded by name.
+    '''
+
+    def __init__(self, *args, folder=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.data = None
+        self.done = False
+        self.path = Path(folder)
+
+        # self.frame_num = 0
+        self.sample_num = 0
+        self.files = []
+
+        if not self.path.exists() or not self.path.is_dir():
+            raise AttributeError('Folder {} does not exist.'.format(self.path))
+
+    def setup(self):
+        pass
+        
+    def saveImgs(self, exts):
+        '''
+        Arg: exts = list of possible file extensions
+        '''
+        self.imgs = []
+        files = {f for f in self.path.iterdir() if f.suffix in exts}
+        files = sorted(list(files))
+        for file in files:
+            img = self.get_sample(file)
+            self.imgs.append(img)
+        self.imgs = np.array(self.imgs)
+        f = h5py.File('output/sample.h5', 'w', libver='latest')
+        f.create_dataset("default", data=self.imgs)
+        f.close()
+
+    def run(self, exts):
+        ''' Triggered at Run
+            Get list of files in the folder and use that as the baseline.
+            Arg: exts = list of possible extensions
+        '''
+        self.total_times = []
+        self.timestamp = []
+        
+        self.files = [f for f in self.path.iterdir() if f.suffix in exts]
+
+        with RunManager(self.name, self.runAcquirer, self.setup, self.q_sig, self.q_comm) as rm:
+            print(rm)
+
+        if '.tif' or '.tiff' in exts:
+            print('Acquire broke, avg time per frame: ', np.mean(self.total_times))
+            print('Acquire got through ', self.sample_num, ' frames')
+        if '.jpg' or '.png' in exts:
+            print('Acquire broke, avg time per image: ', np.mean(self.total_times))
+            print('Acquire got through ', self.sample_num, ' images')
+
+    def runAcquirer(self):
+        ''' Main loop. If there're new files, read and put into store.
+        '''
+        if self.done:
+            pass
+        
+        else:
+            t = time.time()
+            try:
+                obj_id = self.client.put(self.get_sample(self.files[self.sample_num]), 'acq_raw' + str(self.sample_num))
+                self.timestamp.append([time.time(), self.sample_num])
+                self.put([[obj_id, str(self.sample_num)]], save=[True])
+                self.sample_num += 1
+            except Exception as e:
+                logger.error('Acquirer general exception: {}'.format(e))
+            except IndexError as e:
+                pass
+
+            self.total_times.append(time.time() - t)
+        
+        # From bubblewrap demo and FileAcquirer
+        logger.error('Done with all available data: {0}'.format(self.sample_num))
+        self.data = None
+        self.q_comm.put(None)
+        self.done = True  # stay awake in case we get a shutdown signal
+
+    def get_sample(self, file: Path):
+        try:
+            img = imread(file.as_posix())
+        except ValueError as e:
+            img = imread(file.as_posix())
+            logger.error('File ' + file.as_posix() + ' had value error {}'.format(e))
+        return img # [0,0,0, :, :,0]  # Extract first channel in this image set, or tensor/3D array for .jpg/.png
+
 class StimAcquirer(Actor):
     ''' Class to load visual stimuli data from file
         and stream into the pipeline
@@ -119,7 +214,7 @@ class StimAcquirer(Actor):
     def __init__(self, *args, param_file=None, filename=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.param_file = param_file
-        self.filename= filename
+        self.filename = filename
 
     def setup(self):
         self.n= 0
@@ -127,7 +222,7 @@ class StimAcquirer(Actor):
         if os.path.exists(self.filename):
             print('Looking for ', self.filename)
             n, ext = os.path.splitext(self.filename)[:2]
-            if ext== ".txt":
+            if ext == ".txt":
                 # self.stim = np.loadtxt(self.filename)
                 self.stim=[]
                 f = np.loadtxt(self.filename)
