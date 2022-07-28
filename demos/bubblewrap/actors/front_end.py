3a8ed049f081f908623463aed940527e8e90171e
cyzhou1028 <christopher.y.zhou@duke.edu>
2022-07-28 13:22:44 -0400

    added front_end exit functionality

--- demos/bubblewrap/actors/front_end.py
+++ demos/bubblewrap/actors/front_end.py
@@ -11,7 +11,7 @@ from improv.actor import Spike
 from queue import Empty
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
-from PyQt5.QtWidgets import QGridLayout
+from PyQt5.QtWidgets import QGridLayout, QMessageBox
 
 import logging; logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -100,6 +100,20 @@ class FrontEnd(QtWidgets.QMainWindow, improv_bubble.Ui_Demo):
     def _setup(self):
         self.comm.put([Spike.setup()])
         self.visual.setup()
+
+    def closeEvent(self, event):
+        '''Clicked x/close on window
+            Add confirmation for closing without saving
+        '''
+        confirm = QMessageBox.question(self, 'Message', 'Quit without saving?',
+                    QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if confirm == QMessageBox.Yes:
+            self.comm.put([Spike.quit()])
+            # print('Visual broke, avg time per frame: ', np.mean(self.visual.total_times, axis=0))
+            print('Visual got through ', self.visual.frame_num, ' frames')
+            # print('GUI avg time ', np.mean(self.total_times))
+            event.accept()
+        else: event.ignore()
 
 def _call(fnc, *args, **kwargs):
     ''' Call handler for (external) events
