7e1b4de3b72fa22cfd265416f250f6c2aad53f9e
eaogorman <eao21@duke.edu>
2022-08-03 19:19:12 -0400

    Updated scripted_ResNet50.py — JIT-compiled model

--- demos/pytorch/TorchScript/ResNet50_TorchScript.py
+++ demos/pytorch/TorchScript/ResNet50_TorchScript.py
@@ -1,48 +1,29 @@
 import os
+import numpy as np
 import torch
 from torch import nn
+import torchvision.transforms as transforms
 from torchvision.models import resnet50, ResNet50_Weights
-from torchvision import datasets
-from torchvision.models.feature_extraction import create_feature_extractor
-# from torch.utils.data import ToTensor
 
-def main():
+gpu_num = 0
+
+class RN(nn.Module):
     
-    class RN(nn.Module):
-        
-        def __init__(self, weights=ResNet50_Weights.DEFAULT, model=resnet50):
-            super(RN, self).__init__()
-            # For MNIST:
-            # self.param = nn.Parameter(torch.rand(28, 28))
-            self.weights = weights
-            self.model = model(weights=self.weights, progress=True).eval()
-            self.transforms = weights.transforms()
+    def __init__(self):
+        super().__init__()
+        weights = ResNet50_Weights.DEFAULT
+        self.model = resnet50(weights=weights, progress=True).eval()
+        self.transform = weights.transforms()
 
-        def forward(self, x: torch.Tensor) -> torch.Tensor:
-            with torch.no_grad():
-                x = self.transforms(x)
-                return self.model(x)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        with torch.no_grad():
+            x = self.transform(x)
+            return self.model(x)
 
-    model = RN()
-    scripted_model = torch.jit.script(model)
-    # model_fx = create_feature_extractor(model, return_nodes=['add'])
-    # scripted_model = torch.jit.script(model_fx).to(device)
+device = torch.device("cuda:{}".format(gpu_num) if torch.cuda.is_available() else "cpu")
+                
+model = RN().to(device)
+scripted_model = torch.jit.script(model).to(device)
 
-    # For MNIST data example size, one "batch", one image to simulate model seeing one input at a time:
-    example_size = (1, 1, 28, 28)
-    # For CIFAR-10 data example size, one "batch", one image to simulate model seeing one input at a time:
-    # example_size = (1, 3, 32, 32)
-    # Without hard-coding example size...
-    # dataset = datasets.MNIST(root="./data", download=False, transform=transforms.ToTensor())
-
-    example = torch.rand(example_size)
-
-    # Use torch.jit.trace to generate a torch.jit.ScriptModule via tracing
-    traced_model = torch.jit.trace(scripted_model, example)
-
-    # Save the TorchScript model
-    os.makedirs("./models", exist_ok=True)
-    traced_model.save("./models/scripted_ResNet50.pt")
-
-if __name__ == "__main__":
-    main()
+os.makedirs("./models", exist_ok=True)
+scripted_model.save("./models/scripted_ResNet50.pt")
