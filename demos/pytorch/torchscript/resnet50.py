e2546be749c6001659e55d8d7721e3717b3a9877
eaogorman <eao21@duke.edu>
2022-08-03 21:12:15 -0400

    Updated TorchScript models

--- demos/pytorch/torchscript/resnet50.py
+++ demos/pytorch/torchscript/resnet50.py
@@ -2,12 +2,9 @@ import os
 import numpy as np
 import torch
 from torch import nn
-import torchvision.transforms as transforms
 from torchvision.models import resnet50, ResNet50_Weights
 
-gpu_num = 0
-
-class RN(nn.Module):
+class ResNet50(nn.Module):
     
     def __init__(self):
         super().__init__()
@@ -19,11 +16,3 @@ class RN(nn.Module):
         with torch.no_grad():
             x = self.transform(x)
             return self.model(x)
-
-device = torch.device("cuda:{}".format(gpu_num) if torch.cuda.is_available() else "cpu")
-                
-model = RN().to(device)
-scripted_model = torch.jit.script(model).to(device)
-
-os.makedirs("./models", exist_ok=True)
-scripted_model.save("./models/scripted_ResNet50.pt")
