5676c0744e9f28a7b1c11e6e92730ee6ea930999
eaogorman <eao21@duke.edu>
2022-08-04 20:46:02 -0400

    Updated processor.py for PyTorch demo — input dim

--- demos/pytorch/actors/processor.py
+++ demos/pytorch/actors/processor.py
@@ -1,3 +1,4 @@
+import os
 import time
 import numpy as np
 from improv.store import CannotGetObjectError, ObjectNotFoundError
@@ -5,10 +6,13 @@ from queue import Empty
 from improv.actor import Actor, RunManager
 
 import torch
-# import torch.multiprocessing as mp
+import torch.multiprocessing as mp
+from torchvision.io import read_image
 
-import os
-os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+# mp.set_start_method('spawn')
+
+# import os
+# os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 
 import logging; logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -17,42 +21,35 @@ import traceback
 class PyTorchProcessor(Actor):
     # TODO: Update ALL docstrings
     # TODO: Clean commented sections
-    # TODO: add GPU/CPU option as input...
-    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # TODO: Add GPU/CPU option as input...
+    # TODO: Add any relevant q_comm
     ''' 
     '''
 
-    # def __init__(self, *args,  transforms_path=None, **kwargs):
-    def __init__(self, *args,  transforms_path=None, model_path=None, label_path=None, classify=True, **kwargs):
-        super().__init__(*args)
-        logger.info(transforms_path)
-        if transforms_path is None:
-            # logger.error("Must specify a transforms path.")
-            logger.error("Must specify a JIT-compiled transforms path.")
-        else:
-            self.transforms_path = transforms_path
+    def __init__(self, *args, gpu=False, gpu_num=None, model_path=None, classify=False, **kwargs):
+        super().__init__(*args, **kwargs)
+        logger.info(model_path)
         if model_path is None:
-            # logger.error("Must specify a transforms path.")
+            # logger.error("Must specify a model path.")
             logger.error("Must specify a JIT-compiled model path.")
         else:
             self.model_path = model_path
-        if classify is True and label_path is None:
-            logger.error("Must specify a path to image labels.")
-        else:
-            self.label_path = label_path
 
         self.img_num = 0
 
-        # Add cuda:0 ect. as input - device
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        if gpu is True:
+            self.device = torch.device("cuda:{}".format(gpu_num) if torch.cuda.is_available() else "cpu")
+        else:
+            self.device = torch.device("cpu")
 
-        # if classify is True:
-        #     self.classify = True
+        self.classify = classify
 
     def setup(self):
-        ''' Initialize transforms model
+        ''' Initialize model
         '''
-        logger.info('Loading transforms for ' + self.name)
+        # Idk where this should be?
+        os.makedirs("output", exist_ok=True)
+        logger.info('Loading model for ' + self.name)
         self.done = False
         self.dropped_img = []
 
@@ -62,25 +59,9 @@ class PyTorchProcessor(Actor):
 
         t = time.time()
 
-        # self.transforms = torch.jit.load(self.transforms_path).to(self.device)
+        self.model = torch.jit.load(self.model_path).to(self.device)
 
-        self.transforms = torch.jit.load(self.transforms_path).to(self.device).share_memory
-        
-        self.load_transforms_time = time.time() - t
-        print('Time to load transforms: ', self.load_transforms_time*1000.0)
-        with open("output/timing/load_transforms_time.txt", "w") as text_file:
-            text_file.write("%s" % self.load_transforms_time)
-            
-        logger.info('Loading model for ' + self.name)
-
-        t = time.time()
-
-        # import os
-        # os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
-
-        # self.model = torch.jit.load(self.model_path).to(self.device)
-
-        self.model = torch.jit.load(self.model_path).to(self.device).share_memory
+        # self.model = torch.jit.load(self.model_path).to(self.device).share_memory
 
         # num_processes = 4
         # processes = []
@@ -91,24 +72,26 @@ class PyTorchProcessor(Actor):
         # for p in processes:
         #     p.join()
 
-        self.load_model_time = time.time() - t
-        print('Time to load model: ', self.load_model_time*1000.0)
+        load_model_time = time.time() - t
+        print('Time to load model: ', load_model_time*1000.0)
         with open("output/timing/load_model_time.txt", "w") as text_file:
-            text_file.write("%s" % self.load_model_time)
-
-        # return self.transforms, self.model
+            text_file.write("%s" % load_model_time)
 
     def run(self):
         ''' Run the processor continually on input data, e.g.,images
         '''
-        self.img_num = []
         self.get_img_out = []
         self.proc_img_time = []
-        self.put_img_out = []
+        self.to_device = []
         self.inference_time = []
-        self.put_out_time = []
-        self.pred_time = []
-        self.put_pred_time = []
+        self.out_to_np = []
+        self.put_out_store = []
+        self.put_q_out = []
+        if self.classify is True:
+            self.pred_time = []
+            self.put_pred_store = []
+            self.put_pred_out = []
+
         self.total_times = []
         
         with RunManager(self.name, self.runProcess, self.setup, self.q_sig, self.q_comm) as rm:
@@ -119,12 +102,15 @@ class PyTorchProcessor(Actor):
 
         np.savetxt('output/timing/get_img_out.txt', np.array(self.get_img_out))
         np.savetxt('output/timing/process_image_time.txt', np.array(self.proc_img_time))
-        np.savetxt('output/timing/put_image_out.txt', np.array(self.put_img_out))
+        np.savetxt('output/timing/to_device.txt', np.array(self.to_device))
         np.savetxt('output/timing/inference_time.txt', np.array(self.inference_time))
-        np.savetxt('output/timing/put_out_time.txt', np.array(self.put_out_time))
+        np.savetxt('output/timing/out_to_np.txt', np.array(self.out_to_np))
+        np.savetxt('output/timing/put_out_store.txt', np.array(self.put_out_store))
+        np.savetxt('output/timing/put_q_out.txt', np.array(self.put_q_out))
         if self.classify is True:
             np.savetxt('output/timing/prediction_time.txt', np.array(self.pred_time))
-            np.savetxt('output/timing/put_prediction_time.txt', np.array(self.put_pred_time))
+            np.savetxt('output/timing/put_prediction_time.txt', np.array(self.put_pred_store))
+            np.savetxt('output/timing/put_prediction_time.txt', np.array(self.put_pred_out))
         np.savetxt('output/timing/total_times.txt', np.array(self.total_times))
 
     def runProcess(self):
@@ -145,27 +131,33 @@ class PyTorchProcessor(Actor):
                 t1 = time.time()
                 img = self._processImage(img)
                 t2 = time.time()
-                img_obj_id = self.client.put(img, 'proc_img' + str(self.img_num))
-                self.q_out.put([img_obj_id, str(self.img_num)])
-                output = self._runInference(img)
+                output, t_dev = self._runInference(img)
                 t3 = time.time()
-                out_obj_id = self.client.put(output.numpy(), 'output' + str(self.img_num))
-                self.q_out.put([[out_obj_id, str(self.img_num)]])
+                output = output.detach().numpy()
                 t4 = time.time()
+                out_obj_id = self.client.put(output, 'output' + str(self.img_num))
+                t5 = time.time()
+                self.q_out.put([[out_obj_id, str(self.img_num)]])
+                t6 = time.time()
                 if self.classify is True:
                     pred = self._classifyImage(output, self.img_num)
-                    t5 = time.time()
+                    t7 = time.time()
                     pred_obj_id = self.client.put(pred, 'prediction' + str(self.img_num))
-                    # self.put([[pred_obj_id, str(self.img_num)]], save=True)
-                    t6 = time.time()
-                    self.pred_time.append((t5 - t4)*1000.0)
-                    self.put_pred_time.append((t6 - t5)*1000.0)
+                    t8 = time.time()
+                    self.q_out.put([[pred_obj_id, str(self.img_num)]])
+                    t9 = time.time()
+                    self.pred_time.append((t7 - t6)*1000.0)
+                    self.put_pred_store.append((t8 - t7)*1000.0)
+                    self.put_pred_out.append((t9 - t8)*1000.0)
+
 
                 self.get_img_out.append((t1 - t)*1000.0)
                 self.proc_img_time.append((t2 - t1)*1000.0)
-                self.put_img_out.append((t3 - t2)*1000.0)
+                self.to_device.append(t_dev*1000.0)
                 self.inference_time.append((t3 - t2)*1000.0)
-                self.put_out_time.append((t4 - t3)*1000.0)
+                self.out_to_np.append((t4 - t3)*1000.0)
+                self.put_out_store.append((t5 - t4)*1000.0)
+                self.put_q_out.append((t6 - t5)*1000.0)
 
                 self.img_num += 1
             # Insert exceptions here...ERROR HANDLING, SEE ANNE'S ACTORS - from 1p demo
@@ -179,10 +171,10 @@ class PyTorchProcessor(Actor):
                 self.dropped_img.append(self.img_num)
             except Exception as e:
                 logger.error('Processor error: {}: {} during image number {}'.format(type(e).__name__,
-                                                                                            e, self.frame_number))
+                                                                                            e, self.img_num))
                 print(traceback.format_exc())
                 self.dropped_img.append(self.img_num)
-            self.total_times.append(time.time()-t)
+            self.total_times.append((time.time()-t)*1000.0)
         else:
             pass
 
@@ -191,7 +183,6 @@ class PyTorchProcessor(Actor):
         ''' Check to see if we have images for processing
         From basic demo
         '''
-        # t = time.time()
         try:
             # Timeout = 0 ms
             res = self.q_in.get(timeout=0)
@@ -200,28 +191,27 @@ class PyTorchProcessor(Actor):
         except Empty:
             logger.info('No images for processing')
             return None
-        # self.get_in_time = time.time() - t
 
     def _processImage(self, img):
         ''' Load data - here, .jpg image to tensor
         Input is already loaded image from q_in
-        TODO: Time? Above?
         '''
-        # t = time.time()
         if img is None:
             raise ObjectNotFoundError
-        img = self.transforms(img).unsqueeze(dim=0)
+        # img = torch.from_numpy(img).unsqueeze(dim=0)
+        img = read_image(img)
         return img
-
-        # self.load_img_time.append([time.time() - t])
 
     def _runInference(self, data):
         '''
         '''
-        data = data.to(self.device)
+        # TODO: time to device
+        t = time.time()
+        data = data.unsqueeze(dim=0).to(self.device)
+        t_dev = time.time() - t
         with torch.no_grad():
             output = self.model(data).squeeze(dim=0).to(self.device)
-        return output
+        return output, t_dev
 
     # def _classifyImage(self, labels=('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')):
     #     '''
@@ -235,14 +225,3 @@ class PyTorchProcessor(Actor):
 
 class NaNDataException(Exception):
     pass
-
-
-
-
-
-
-
-
-   
-
-  
