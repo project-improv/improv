0ed696e06fa8177a1b8b11c848216642cb7af7cb
eaogorman <eao21@duke.edu>
2022-08-04 20:39:02 -0400

    Delete absolute paths for CIFAR10.py

--- demos/pytorch/scripts/CIFAR10.py
+++ demos/pytorch/scripts/CIFAR10.py
@@ -7,19 +7,18 @@ from torchvision.utils import save_image
 # Run in ~/improv/demos/pytorch
 def main():
 
-    root = "/home/eao21/improv/demos/pytorch/data/"
-    os.makedirs(root + "/CIFAR10/images", exist_ok=True)
-    os.makedirs(root + "/CIFAR10/labels", exist_ok=True)
+    os.makedirs("data/CIFAR10/images", exist_ok=True)
+    os.makedirs("data/CIFAR10/labels", exist_ok=True)
 
     batch_size = 200
     
-    dataset = datasets.CIFAR10(root=root, download=True, transform=ToTensor())
+    dataset = datasets.CIFAR10(root="data", download=True, transform=ToTensor())
     dataset = iter(DataLoader(dataset, batch_size=batch_size))
     
     for i in range(batch_size):
         x, label = (next(dataset))
-        save_image(x[0], root + "CIFAR10/images/{}.jpg".format(i))
-        with open(root + "CIFAR10/labels/{}.txt".format(i), "w") as text_file:
+        save_image(x[0], "data/CIFAR10/images/{}.jpg".format(i))
+        with open("data/CIFAR10/labels/{}.txt".format(i), "w") as text_file:
             text_file.write("%s" % label)
 
 if __name__ == "__main__":
