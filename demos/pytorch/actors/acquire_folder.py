ac9196f1e73034816a0fcef64aeac7a25b0c964e
eaogorman <eao21@duke.edu>
2022-08-18 02:07:08 -0400

    Corrected Acquirer and Processor for timing output

--- demos/pytorch/actors/acquire_folder.py
+++ demos/pytorch/actors/acquire_folder.py
@@ -4,6 +4,7 @@ import os
 # import random
 import numpy as np
 from skimage.io import imread
+import json
 
 # For FolderAcquirer
 from pathlib import Path
@@ -13,6 +14,13 @@ from improv.actor import Actor, RunManager
 
 import logging; logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+# FOR TIMING:
+# t = time.time()
+# [some process]
+# torch.cuda.current_stream().synchronize()
+# t = time.time()
+# WHICH IS BETTER???
 
 # Modified from ~/improv/demos/neurofinder/actors/acquire_folder.py
 # Maybe rename - ImageAcquirer? 
@@ -24,11 +32,11 @@ class FolderAcquirer(Actor):
         If there're multiple files, files are loaded by name.
     '''
 
-    def __init__(self, *args, folder=None, exts=None, classify=False, label_folder=None, out_path=None, **kwargs):
+    def __init__(self, *args, sleep_time=.5, folder=None, exts=None, classify=False, label_folder=None, out_path=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.done = False
 
-        self.sample_num = 0
+        self.img_num = 0
 
         self.classify = classify
 
@@ -44,14 +52,16 @@ class FolderAcquirer(Actor):
         else:
             self.exts = exts
 
-        if self.classify and label_folder is None:
+        if self.classify is True and label_folder is None:
             logger.error('Must specify folder of labels.')
-        elif self.classify and label_folder is not None:
+        elif self.classify is True and label_folder is not None:
             self.lab_path = Path(label_folder)
             if not self.lab_path.exists() or not self.lab_path.is_dir():
                 raise AttributeError('Label folder {} does not exist.'.format(self.lab_path))
         
         self.out_path = out_path
+
+        self.sleep_time = sleep_time
 
     def setup(self):
         os.makedirs(self.out_path, exist_ok=True)
@@ -59,9 +69,11 @@ class FolderAcquirer(Actor):
         self.files = [f.as_posix() for f in self.path.iterdir() if f.suffix in self.exts]
         self.files.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
 
-        if self.classify and self.lab_path is not None:
+        if self.classify is True and self.lab_path is not None:
             self.lab_files = [f.as_posix() for f in self.lab_path.iterdir()]
             self.lab_files.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+
+        self.n_imgs = len(self.files)
         
     # def saveImgs(self):
     #     '''
@@ -71,10 +83,10 @@ class FolderAcquirer(Actor):
     #     files = [f.as_posix() for f in self.path.iterdir() if f.suffix in self.exts]
     #     files.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
     #     for file in files:
-    #         img = self.get_sample(file)
+    #         img = self.get_img(file)
     #         self.imgs.append(img)
     #     self.imgs = np.array(self.imgs)
-    #     f = h5py.File('output/sample.h5', 'w', libver='latest')
+    #     f = h5py.File('output/img.h5', 'w', libver='latest')
     #     f.create_dataset("default", data=self.imgs)
     #     f.close()
 
@@ -84,31 +96,42 @@ class FolderAcquirer(Actor):
             Arg: exts = list of possible extensions
         '''
         self.put_img_time = []
+        self.put_out_time = []
         self.timestamp = []
-        self.total_times = []
+        self.acq_total_times = []
 
-        if self.classify and self.lab_path is not None:
-            self.put_both_time = []
+        if self.classify is True and self.lab_path is not None:
+            self.put_lab_time = []
             self.lab_timestamp = []
 
         with RunManager(self.name, self.runAcquirer, self.setup, self.q_sig, self.q_comm) as rm:
             print(rm)
 
+        # if self.img_num == 200:
+        #     with open(self.out_path + "acq_vars.txt", "w") as f:
+        #         print(vars(self), file=f)
+        #         f.close()
+
         if '.tif' or '.tiff' in self.exts:
-            print('Acquire broke, avg time per frame: ', np.mean(self.total_times))
-            print('Acquire got through ', self.sample_num, ' frames')
+            print('Acquire broke, avg time per frame: ', np.mean(self.acq_total_times))
+            print('Acquire got through ', self.img_num, ' frames')
         if '.jpg' or '.png' in self.exts:
-            print('Acquire broke, avg time per image: ', np.mean(self.total_times))
-            print('Acquire got through ', self.sample_num, ' images')
+            print('Acquire broke, avg time per image: ', np.mean(self.acq_total_times))
+            print('Acquire got through ', self.img_num, ' images')
 
-        np.savetxt(self.out_path + 'put_img_time.txt', np.array(self.put_img_time))
-        np.savetxt(self.out_path + 'acquire_timestamp.txt', np.array(self.timestamp))
-        
-        if self.classify and self.lab_path is not None:
-            np.savetxt(self.out_path + 'put_img_lab_time.txt', np.array(self.put_both_time))
-            np.savetxt(self.out_path + 'acquire_lab_timestamp.txt', np.array(self.lab_timestamp))
+        # np.savetxt(self.out_path + 'put_img_time.txt', np.array(self.put_img_time))
+        # np.savetxt(self.out_path + 'acquire_timestamp.txt', np.array(self.timestamp))
+        # np.savetxt(self.out_path + 'put_out_time.txt', np.array(self.put_out_time))
+        # logger.error('ACQ1: TESTTESTTEST!!!')
 
-        np.savetxt(self.out_path + 'total_times.txt', np.array(self.total_times))
+        # if self.classify is True and self.lab_path is not None:
+        #     np.savetxt(self.out_path + 'put_lab_time.txt', np.array(self.put_lab_time))
+        #     np.savetxt(self.out_path + 'acquire_lab_timestamp.txt', np.array(self.lab_timestamp))
+        #     logger.error('ACQ2: TESTTESTTEST!!!')
+
+        # np.savetxt(self.out_path + 'acq_total_times.txt', np.array(self.acq_total_times))
+
+        print('Done saving out!')
         
     def runAcquirer(self):
         ''' Main loop. If there're new files, read and put into store.
@@ -116,42 +139,52 @@ class FolderAcquirer(Actor):
         if self.done:
             pass
         
-        elif self.sample_num != len(self.files):
+        elif self.img_num != len(self.files):
             t = time.time()
             try:
                 t1 = time.time()
-                img_obj_id = self.client.put(self.get_sample(self.files[self.sample_num]), 'acq_raw' + str(self.sample_num))
-                # img = self.client.getID(img_obj_id)
-                # print(img)
-                # self.timestamp.append([time.time()*1000.0, self.sample_num])
-                # if self.classify is False:
-                self.q_out.put([img_obj_id, str(self.sample_num)])
-                # self.put_img_time.append(time.time() - t1)*1000.0
+                img_obj_id = self.client.put(self.get_img(self.files[self.img_num]), 'acq_raw' + str(self.img_num))
+                self.timestamp.append([time.time()*1000.0, self.img_num])
+                self.put_img_time.append((time.time() - t1)*1000.0)
+                t2 = time.time()
             # Get lab at same time as image? Simulate human labeling image?
-                if self.classify and self.lab_path is not None:
-                    t2 = time.time()
-                    lab_obj_id = self.client.put(self.get_label(self.lab_files[self.sample_num]), 'acq_lab' + str(self.sample_num))
-                    self.lab_timestamp.append([time.time()*1000.0, self.sample_num])
-                    self.q_out.put([img_obj_id, lab_obj_id, str(self.sample_num)])
-                    # self.put_both_time = (time.time() - t2)*1000.0
-                time.sleep(.05)
-                self.sample_num += 1
+                if self.classify is True and self.lab_path is not None:
+                    t3 = time.time()
+                    lab_obj_id = self.client.put(self.get_label(self.lab_files[self.img_num]), 'acq_lab' + str(self.img_num))
+                    self.put_lab_time.append((time.time() - t3)*1000.0)
+                    self.lab_timestamp.append([time.time()*1000.0, self.img_num])
+                    t4 = time.time()
+                    self.q_out.put([img_obj_id, lab_obj_id, str(self.img_num), self.n_imgs])
+                    self.put_out_time.append((time.time() - t4)*1000.0)
+                else:
+                    self.q_out.put([img_obj_id, str(self.img_num), self.n_imgs])
+                    self.put_out_time.append((time.time() - t2)*1000.0)
+         
+                self.img_num += 1
+                self.acq_total_times.append((time.time() - t)*1000.0)
+
+                if self.img_num == self.n_imgs:
+                    tmp = vars(self)
+                    with open(self.out_path + "acq_vars.txt", "w+") as f:
+                        d = {str(item): str(tmp[item]) for item in tmp}
+                        print(d, file=f)
+                        # print(vars(self), file=f)
+                        f.close()
+                        # print('Done saving!')
+
             except Exception as e:
                 logger.error('Acquirer general exception: {}'.format(e))
             except IndexError as e:
                 pass
 
-            # time.sleep(self.framerate)  # pretend framerate
-            # self.total_times.append((time.time() - t)*1000.0)
-        
         # From bubblewrap demo and FileAcquirer
-        if self.sample_num == len(self.files):
-            logger.error('Done with all available data: {}'.format(self.sample_num))
+        if self.img_num == self.n_imgs:
+            logger.error('Done acquiring all available data: {}'.format(self.img_num))
             self.data = None
             self.q_comm.put(None)
             self.done = True  # stay awake in case we get a shutdown signal
 
-    def get_sample(self, file):
+    def get_img(self, file):
         '''
         '''
         try:
@@ -167,7 +200,7 @@ class FolderAcquirer(Actor):
         '''
         TODO: Maybe can make one line? Speed up? User input -> label str
         '''
-        label_file = open(label_file, "r")
-        label = label_file.read()
-        label_file.close()
+        with open(label_file, "r") as f:
+            label = int(f.read())
+            f.close()
         return label
--- demos/pytorch/actors/processor.py
+++ demos/pytorch/actors/processor.py
@@ -1,16 +1,19 @@
 import os
+import json
 import time
 import numpy as np
 from improv.store import CannotGetObjectError, ObjectNotFoundError
 from queue import Empty
 from improv.actor import Actor, RunManager
 
-from pyarrow._plasma import PlasmaObjectExists, ObjectNotAvailable, ObjectID
-
 import torch
+from scipy.special import softmax
+import pickle
 
 import logging; logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+logging.getLogger('PIL').setLevel(logging.WARNING)
+
 import traceback
 
 class PyTorchProcessor(Actor):
@@ -33,9 +36,12 @@ class PyTorchProcessor(Actor):
         self.img_num = 0
 
         if gpu is True:
-            self.device = torch.device("cuda:{}".format(gpu_num) if torch.cuda.is_available() else "cpu")
+            # self.device = torch.device("cuda:{}".format(gpu_num) if torch.cuda.is_available() else "cpu")
+            self.device = torch.device("cuda:{}".format(gpu_num))
+            torch.jit.fuser('fuser2')
         else:
             self.device = torch.device("cpu")
+            torch.jit.fuser('fuser1')
 
         self.classify = classify
 
@@ -49,22 +55,44 @@ class PyTorchProcessor(Actor):
     def setup(self):
         ''' Initialize model
         '''
+        if self.classify is True and self.labels is not None:
+            logger.info('Loading label names')
+            with open(self.labels, "r") as label_file:
+                self.labels = json.loads(label_file.read())
+                label_file.close()
+
         # Idk where this should be?
         os.makedirs(self.out_path, exist_ok=True)
         logger.info('Loading model for ' + self.name)
         self.done = False
         self.dropped_img = []
 
-        t = time.time()
-
-        self.model = torch.jit.load(self.model_path).to(self.device)
+        t = time.time()        
+        self.model = torch.jit.load(self.model_path).eval().to(self.device)
+        torch.cuda.synchronize()
+        load_model_time = (time.time() - t)*1000.0
 
         # self.model = torch.jit.load(self.model_path).to(self.device).share_memory
 
-        load_model_time = time.time() - t
-        print('Time to load model: ', load_model_time*1000.0)
+        print('Time to load model: ', load_model_time)
         with open(self.out_path + "load_model_time.txt", "w") as text_file:
             text_file.write("%s" % load_model_time)
+            text_file.close()
+
+        t = time.time()
+        sample_input = torch.rand(size=(1, 3, 224, 224), device=self.device)
+        # self.model = torch.jit.optimize_for_inference(self.model)
+        # Still have the high first run??? Why even though warmup in setup?
+        with torch.no_grad():
+            for _ in range(5):
+                self.model(sample_input)
+                torch.cuda.synchronize()
+                
+        warmup_time = (time.time() - t)*1000.0
+        print('Time to warmup: ', warmup_time)
+        with open(self.out_path + "warmup_time.txt", "w") as text_file:
+            text_file.write("%s" % warmup_time)
+            text_file.close()
 
     def run(self):
         ''' Run the processor continually on input data, e.g.,images
@@ -73,34 +101,56 @@ class PyTorchProcessor(Actor):
         self.proc_img_time = []
         self.to_device = []
         self.inference_time = []
-        # self.out_to_np = []
-        # self.put_out_store = []
-        # self.put_q_out = []
-        # if self.classify is True:
-        #     self.pred_time = []
-        #     self.put_pred_store = []
-        #     self.put_pred_out = []
+        self.out_to_np = []
+        self.put_out_store = []
+        self.put_q_out = []
+        if self.classify is True:
+            self.pred_time = []
+            self.put_pred_store = []
+            self.put_pred_out = []
+            self.true_label = []
+            self.pred_label = []
+            self.percent = []
+            self.top_five = []
 
         self.total_times = []
-        
+
+        # print('TEST!!!')
+
         with RunManager(self.name, self.runProcess, self.setup, self.q_sig, self.q_comm, runStore=self._getStoreInterface()) as rm:
             print(rm)
+
+        # logger.error('1: TEST!!!TEST!!!TEST!!!TEST!!!')
+
+        if self.img_num == 300:
+            with open(self.out_path + "proc_vars_rm.txt", "w") as f:
+                print(vars(self), file=f)
+                f.close()
+
+        # logger.error('2: TEST!!!TEST!!!TEST!!!TEST!!!')
         
         print('Processor broke, avg time per image: ', np.mean(self.total_times, axis=0))
         print('Processor got through ', self.img_num, ' images')
 
-        np.savetxt(self.out_path + 'get_img_out.txt', np.array(self.get_img_out))
-        np.savetxt(self.out_path + 'process_img_time.txt', np.array(self.proc_img_time))
-        np.savetxt(self.out_path + 'to_device.txt', np.array(self.to_device))
-        np.savetxt(self.out_path + 'inference_time.txt', np.array(self.inference_time))
+        # np.savetxt(self.out_path + 'get_img_out.txt', np.array(self.get_img_out))
+
+        # logger.error('3: TEST!!!TEST!!!TEST!!!TEST!!!')
+
+        # np.savetxt(self.out_path + 'process_img_time.txt', np.array(self.proc_img_time))
+        # np.savetxt(self.out_path + 'to_device.txt', np.array(self.to_device))
+        # np.savetxt(self.out_path + 'inference_time.txt', np.array(self.inference_time))
         # np.savetxt(self.out_path + 'out_to_np.txt', np.array(self.out_to_np))
         # np.savetxt(self.out_path + 'put_out_store.txt', np.array(self.put_out_store))
         # np.savetxt(self.out_path + 'put_q_out.txt', np.array(self.put_q_out))
         # if self.classify is True:
         #     np.savetxt(self.out_path + 'prediction_time.txt', np.array(self.pred_time))
-        #     np.savetxt(self.out_path + 'put_prediction_time.txt', np.array(self.put_pred_store))
-        #     np.savetxt(self.out_path + 'put_prediction_time.txt', np.array(self.put_pred_out))
-        np.savetxt(self.out_path + 'total_times.txt', np.array(self.total_times))
+        #     np.savetxt(self.out_path + 'put_prediction_store.txt', np.array(self.put_pred_store))
+        #     np.savetxt(self.out_path + 'put_prediction_dict.txt', np.array(self.put_pred_out))
+        #     logger.error('4: TEST!!!TEST!!!TEST!!!TEST!!!')
+        # np.savetxt(self.out_path + 'put_prediction_out.txt', np.array(self.put_q_out))
+        # np.savetxt(self.out_path + 'total_times.txt', np.array(self.total_times))
+
+        # print('Done saving out!')
 
     def runProcess(self):
         ''' Run process. Runs once per image.
@@ -112,6 +162,10 @@ class PyTorchProcessor(Actor):
         ids = self._checkInput()
 
         if ids is not None:
+            if self.classify is True:
+                n_imgs = ids[3]
+            else:
+                n_imgs = ids[2]
             t = time.time()
             self.done = False
             try:
@@ -119,46 +173,59 @@ class PyTorchProcessor(Actor):
                 img = self.client.getID(ids[0])
                 t1 = time.time()
                 img = self._processImage(img)
-                print(img)
+                output, t_dev, t_inf = self._runInference(img)
                 t2 = time.time()
-                output, t_dev = self._runInference(img)
-                print(t_dev)
-                print(output)
-                time.sleep(.01)
+                # Want to avoid copy to CPU to detach...detach first?
+                features = output[0].to('cpu').numpy()
+                predictions = output[1].to('cpu').numpy()
                 t3 = time.time()
-                # # Necessary? Time? Optimize storage?
-                # img.detach()
-                # output = output.detach().numpy()
-                # t4 = time.time()
-                # out_obj_id = self.client.put(output, 'output' + str(self.img_num))
-                # t5 = time.time()
-                # # if self.classify is False:
-                # #     self.q_out.put([[out_obj_id, str(self.img_num)]])
-                # t6 = time.time()
-                # if self.classify is True:
-                #     # Do number correct in visual? Or output w/pred?
-                #     # Compare label in and label out, add to counter if correct, keep note of number for each class...but can compare list as both fill in, frame-by-frame? Updating confusion matrix?
-                #     pred = self._classifyImage(output, self.labels)
-                #     t7 = time.time()
-                #     pred_obj_id = self.client.put(pred, 'prediction' + str(self.img_num))
-                #     t8 = time.time()
-                #     self.q_out.put([[out_obj_id, pred_obj_id, str(self.img_num)]])
-                #     t9 = time.time()
-                #     self.pred_time.append((t7 - t6)*1000.0)
-                #     self.put_pred_store.append((t8 - t7)*1000.0)
-                #     self.put_pred_out.append((t9 - t8)*1000.0)
+                feat_obj_id = self.client.put(features, 'features' + str(self.img_num))
+                pred_obj_id = self.client.put(predictions, 'predictions' + str(self.img_num))
+                t4 = time.time()
+                if self.classify is True:
+                # Do number correct in visual? Or output w/pred?
+                # Compare label in and label out, add to counter if correct, keep note of number for each class...but can compare list as both fill in, frame-by-frame? Updating confusion matrix?
+                    pred_label, percent, top_five = self._classifyImage(predictions, self.labels)
+                    t5 = time.time()
+                    lab_obj_id = self.client.put(pred_label, 'prediction' + str(self.img_num))
+                    percent_obj_id = self.client.put(percent, 'percent' + str(self.img_num))
+                    top_obj_id = self.client.put(top_five, 'top_five' + str(self.img_num))
+                    t6 = time.time()
+                    self.true_label.append(self.client.getID(ids[1]))
+                    self.pred_label.append(pred_label)
+                    self.percent.append(percent)
+                    self.top_five.append(top_five)
+                    # AttributeError: 'NoneType' object has no attribute 'put'
+                    # self.q_out.put([feat_obj_id, pred_obj_id, lab_obj_id, percent_obj_id, top_obj_id, str(self.img_num)])
+                    self.put_q_out.append((time.time() - t6)*1000.0)
+                    self.pred_time.append((t5 - t4)*1000.0)
+                    self.put_pred_store.append((t6 - t5)*1000.0)
                     # Visualize updated accuracy...update every few images? OR every image?
                     # Confusion matrix that updates w/incoming data
+                else:
+                    # self.q_out.put([out_obj_id, str(self.img_num)])
+                    self.put_q_out.append((time.time() - t4)*1000.0)
 
                 self.get_img_out.append((t1 - t)*1000.0)
                 self.proc_img_time.append((t2 - t1)*1000.0)
                 self.to_device.append(t_dev*1000.0)
-                self.inference_time.append((t3 - t2)*1000.0)
-                # self.out_to_np.append((t4 - t3)*1000.0)
-                # self.put_out_store.append((t5 - t4)*1000.0)
-                # self.put_q_out.append((t6 - t5)*1000.0)
+                self.inference_time.append(t_inf*1000.0)
+                self.out_to_np.append((t3 - t2)*1000.0)
+                self.put_out_store.append((t4 - t3)*1000.0)
 
                 self.img_num += 1
+                self.total_times.append((time.time() - t)*1000.0)
+
+                if self.img_num == n_imgs:
+                #    self.pred_out = {'true_label': self.true_label, 'pred_label': self.pred_label, 'percent': self.percent, 'top_five': self.top_five}
+                    tmp = vars(self)
+                    with open(self.out_path + "proc_vars.txt", "w+") as f:    
+                        d = {str(item): str(tmp[item]) for item in tmp}
+                        print(d, file=f)
+                        # print(vars(self), file=f)
+                        f.close()
+                        # print('Done saving!')
+
             # Insert exceptions here...ERROR HANDLING, SEE ANNE'S ACTORS - from 1p demo
             except ObjectNotFoundError:
                 logger.error('Processor: Image {} unavailable from store, dropping'.format(self.img_num))
@@ -173,9 +240,22 @@ class PyTorchProcessor(Actor):
                                                                                             e, self.img_num))
                 print(traceback.format_exc())
                 self.dropped_img.append(self.img_num)
-            self.total_times.append((time.time()-t)*1000.0)
+            self.total_times.append((time.time() - t)*1000.0)
+
+            # if self.img_num == len(self.files):
+            #     with open(self.out_path + "proc_vars.txt", "w") as f:
+            #         print(vars(self), file=f)
+            #         f.close()
         else:
             pass
+
+        # From bubblewrap demo and FileAcquirer
+        if self.img_num == n_imgs:
+            logger.error('Done processing all available data: {}'.format(self.img_num))
+            self.data = None
+            self.q_comm.put(None)
+            self.done = True  # stay awake in case we get a shutdown signal
+
 
 # Should the following technically be internal?
     def _checkInput(self):
@@ -208,18 +288,28 @@ class PyTorchProcessor(Actor):
         '''
         t = time.time()
         data = data.to(self.device)
+        torch.cuda.synchronize()        
         to_device = time.time() - t
         with torch.no_grad():
             # Would we want to squeeze for batch size > 1?
-            output = self.model(data).to(self.device)
-        return output, to_device
+            t = time.time()
+            output = self.model(data)
+            torch.cuda.synchronize()
+            inf_time = time.time() - t
+
+        return output, to_device, inf_time
         
-    def _classifyImage(self, output, labels=('plane', 'car', 'bird', 'cat', 'deer' 'dog', 'frog', 'horse', 'ship', 'truck')):
+    def _classifyImage(self, predictions, labels):
         '''
+        output = features (1, 1000), predictions(1, 10)
         '''
-        _, predicted = np.argmax(output.data, dim=1)
+        index = np.argmax(predictions, axis=1)[0]  
+        percentage = (softmax(predictions) * 100)[0]
+
+        indices = np.argsort(predictions, axis=1)[::-1][0]
+        top_five = [(labels[idx], percentage[idx].item()) for idx in indices[:5]]
         
-        return labels[predicted]
+        return labels[index], percentage[index], top_five
 
 class NaNDataException(Exception):
     pass
