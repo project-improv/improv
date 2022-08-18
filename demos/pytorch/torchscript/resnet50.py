acc641a19e25178c2ca7755ae7fe74af641d5677
eaogorman <eao21@duke.edu>
2022-08-18 02:08:57 -0400

    Updated resnet50.py — device and predictions

--- demos/pytorch/torchscript/resnet50.py
+++ demos/pytorch/torchscript/resnet50.py
@@ -5,19 +5,25 @@ from torchvision.models import resnet50, ResNet50_Weights
 
 class ResNet50(nn.Module):
     
-    def __init__(self):
+    def __init__(self, device):
         super().__init__()
+        self.device = device
         weights = ResNet50_Weights.DEFAULT
-        self.model = resnet50(weights=weights, progress=True).eval()
+        self.model = resnet50(weights=weights, progress=True).eval().to(self.device)
+
+        self.classifier = nn.Linear(1000, 10)
+
         self.transform = weights.transforms()
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         with torch.no_grad():
+            x = x.to(self.device)
             x = self.transform(x)
-            return self.model(x)
-                
-model = ResNet50()
-scripted_model = torch.jit.script(model)
+            features = self.model(x)
+            predictions = self.classifier(features)
 
+            return features, predictions
+            
 os.makedirs("models", exist_ok=True)
-scripted_model.save("models/ResNet50.pt")
+cpu_model = torch.jit.script(ResNet50('cpu')).save("models/ResNet50_CPU.pt")
+gpu_model = torch.jit.script(ResNet50('cuda')).save("models/ResNet50_GPU.pt")
