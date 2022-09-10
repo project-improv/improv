24042d65fc493e071f08f7b4b1b1ae731ce15240
eaogorman <eao21@duke.edu>
2022-09-09 22:10:18 -0400

    Renamed PyTorch demo to CNN demo

---
+++ demos/cnn/cnn_demo.py
@@ -0,0 +1,26 @@
+import logging
+# Matplotlib is overly verbose by default
+logging.getLogger("matplotlib").setLevel(logging.INFO)
+
+if __name__ == '__main__':
+    '''
+    '''
+    from improv.nexus import Nexus
+
+    loadFile = './cnn_demo.yaml'
+
+    nexus = Nexus('Nexus')
+    nexus.createNexus(file=loadFile)
+
+# All modules needed have been imported
+# so we can change the level of logging here
+# import logging
+# import logging.config
+# logging.config.dictConfig({
+#     'version': 1,
+#     'disable_existing_loggers': True,
+# })
+# logger = logging.getLogger("improv")
+# logger.setLevel(logging.INFO)
+
+    nexus.startNexus()
