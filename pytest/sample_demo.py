355eb5fbfe546d6b0012722dd1ab1176f5701f9f
John Pearson <jmxpearson@gmail.com>
2022-11-15 13:52:33 -0500

    establishing minimal dependency list
    - /env/improv.yml has a minimal set of dependencies to run
      /pytest/sample_demo.py
    - sample_demo.py has been changed to use best practice if __name__
      (without this, demo does not run)

--- pytest/sample_demo.py
+++ pytest/sample_demo.py
@@ -1,10 +1,13 @@
-from improv.nexus import Nexus
 
 import logging
 logging.getLogger("matplotlib").setLevel(logging.WARNING)
 
-loadFile = "./configs/sample_config.yaml"
 
-nexus = Nexus("Sample")
-nexus.createNexus(file=loadFile)
-nexus.startNexus()
+if __name__ == '__main__':
+    from improv.nexus import Nexus
+
+    loadFile = "./configs/sample_config.yaml"
+
+    nexus = Nexus("Sample")
+    nexus.createNexus(file=loadFile)
+    nexus.startNexus()
