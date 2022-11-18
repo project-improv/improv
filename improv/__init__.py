93928bd3f9366e1f1b6d38919ac9cc7724c6e734
John Pearson <jmxpearson@gmail.com>
2022-11-18 09:22:16 -0500

    adding entry point for code
    improv <configfile>.yml now calls basic_invocation in __init__.py
    This will allow us to rewrite demos to use simple command-line
    invocations

--- improv/__init__.py
+++ improv/__init__.py
@@ -1,1 +1,18 @@
 
+def basic_invocation():
+    import argparse
+    import logging
+
+    from improv.nexus import Nexus
+
+    logging.getLogger("matplotlib").setLevel(logging.WARNING)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_file", help="Name of a YAML configuration file")
+    args = parser.parse_args()
+
+    loadfile = args.config_file
+
+    nexus = Nexus("Sample")
+    nexus.createNexus(file=loadfile)
+    nexus.startNexus()
