84bf339a7ba09745a881a36c5613441ffeae020f
John Pearson <jmxpearson@gmail.com>
2022-11-21 09:08:05 -0500

    command line now supports --actor-path
    - this argument allows users to specify a search path at which actors
      relevant to the config file may be found

--- improv/__init__.py
+++ improv/__init__.py
@@ -1,3 +1,5 @@
+import sys
+import os.path
 
 def default_invocation():
     import argparse
@@ -8,10 +10,16 @@ def default_invocation():
     logging.getLogger("matplotlib").setLevel(logging.WARNING)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("config_file", help="Name of a YAML configuration file")
+    parser.add_argument("config", help="name of a YAML configuration file")
+    parser.add_argument("--actor-path", help="search path to add to sys.path when looking for actors; defaults to the directory containing <config>")
     args = parser.parse_args()
 
-    loadfile = args.config_file
+    if not args.actor_path:
+        sys.path.append(os.path.dirname(args.config))
+    else:
+        sys.path.append(os.path.abspath(args.actor_path))
+
+    loadfile = args.config
 
     nexus = Nexus("Sample")
     nexus.createNexus(file=loadfile)
