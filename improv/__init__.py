9afe05ede654cebfc78a39ec3c9dba5dc31f6ced
John Pearson <jmxpearson@gmail.com>
2022-12-28 13:33:52 -0500

    allowing for multiple actor paths
    - users can specify multiple paths to actors by repeating the
      --actor-path option
    - new tests (and test fixture) added

--- improv/__init__.py
+++ improv/__init__.py
@@ -5,8 +5,8 @@ import click
 from improv.nexus import Nexus
 
 @click.command()
+@click.option('-a', '--actor-path', type=click.Path(exists=True, resolve_path=True), multiple=True, default=[''], help="search path to add to sys.path when looking for actors; defaults to the directory containing CONFIGFILE")
 @click.argument('configfile', type=click.Path(exists=True, dir_okay=False, resolve_path=True))
-@click.option('--actor-path', type=click.Path(exists=True, resolve_path=True), default='', help="search path to add to sys.path when looking for actors; defaults to the directory containing CONFIGFILE")
 def default_invocation(configfile, actor_path):
     """
     Function provided as an entry point for command-line usage. Invoke using
@@ -17,7 +17,7 @@ def default_invocation(configfile, actor_path):
     if not actor_path:
         sys.path.append(os.path.dirname(configfile))
     else:
-        sys.path.append(actor_path)
+        sys.path.extend(actor_path)
     click.echo(configfile)
 
     nexus = Nexus("Sample")
