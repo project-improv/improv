4f1b512d1025817157eee67e2821e4f8de5423e7
John Pearson <jmxpearson@gmail.com>
2023-01-05 14:13:38 -0500

    more tests for cli

--- pytest/test_cli.py
+++ pytest/test_cli.py
@@ -31,6 +31,7 @@ def test_multiple_actor_path():
                                                 ])
 def test_can_override_ports(mode, flag, expected):
     file = 'configs/blank_file.yaml'
+    localhost = "127.0.0.1:"
     params = {'-c': 'control_port',
                 '-o': 'output_port',
                 '-s': 'server_port',
@@ -39,10 +40,47 @@ def test_can_override_ports(mode, flag, expected):
 
     if mode in ['run', 'server']:
         args = cli.parse_cli_args([mode, flag, expected, file])
+        assert vars(args)[params[flag]] == int(expected)
     else:
         args = cli.parse_cli_args([mode, flag, expected])
-    assert vars(args)[params[flag]] == int(expected)
+        assert vars(args)[params[flag]] == localhost + expected
+
+@pytest.mark.parametrize("mode,flag,expected", [('run', '-c', "127.0.0.1:6000"), 
+                                                ('run', '-o', "-6000"),
+                                                ('run', '-l', str(cli.MAX_PORT + 1)),
+                                                ('server', '-c', "127.0.0.1:6000"), 
+                                                ('server', '-o', "-6000"),
+                                                ('server', '-l', str(cli.MAX_PORT + 1)),
+                                                ])
+def test_non_port_is_error(mode, flag, expected):
+    file = 'configs/blank_file.yaml'
+    with pytest.raises(SystemExit):
+        cli.parse_cli_args([mode, flag, expected, file])
+
+@pytest.mark.parametrize("mode,flag,expected", [('client', '-c', "111.127.0.0.1:6000"), 
+                                                ('client', '-s', "127.0.1:6000"),
+                                                ('client', '-s', "-6000"),
+                                                ('client', '-l', str(cli.MAX_PORT + 1)),
+                                                ('client', '-l', "127.0.0.1"),
+                                                ('client', '-l', "127.0.0.1:"),
+                                                ('client', '-l', "127.0.0.1:" + str(cli.MAX_PORT)),
+                                                ])
+def test_non_ip_is_error(mode, flag, expected):
+    with pytest.raises(SystemExit):
+        cli.parse_cli_args([mode, flag, expected])
+
+@pytest.mark.parametrize("mode,flag,expected", [('client', '-c', "127.0.0.1:6000"), 
+                                                ('client', '-s', "155.4.4.3:4000"),
+                                                ])
+def test_can_override_ip(mode, flag, expected):
+    params = {'-c': 'control_port',
+                '-o': 'output_port',
+                '-s': 'server_port',
+                '-l': 'logging_port'
+            }
+    args = cli.parse_cli_args([mode, flag, expected])
+    assert vars(args)[params[flag]] == expected
 
 
-# TODO: test ports vs addresses
 # TODO: test individual functions
+# TODO: convert tests in tui.py to pytest
