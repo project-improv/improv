7487e03df7c072beb159faf45524a32c9fb4e1cb
John Pearson <jmxpearson@gmail.com>
2023-01-07 22:33:40 -0500

    tests passing

--- pytest/test_tui.py
+++ pytest/test_tui.py
@@ -26,62 +26,33 @@ def logger(ports):
     yield logger
 
 @pytest.fixture
-def sockets(ports):
+async def sockets(ports):
     context = zmq.Context()
     ctrl_socket = context.socket(REP)
     ctrl_socket.bind("tcp://*:%s" % ports[0])
     out_socket = context.socket(PUB)
     out_socket.bind("tcp://*:%s" % ports[1])
-    return (ctrl_socket, out_socket)
-
-
-@pytest.fixture
-async def backend(sockets):
-    """
-    Fake program to be controlled by TUI.
-    """
-    socket = sockets[0]  # control socket
-    msg = await socket.recv_multipart()
-    yield socket.send_string("received")
-
-@pytest.fixture
-async def publish(sockets):
-    """
-    Set up a fake server to publish messages for testing.
-    """
-    pubsocket = sockets[1]  # output socket
-    yield pubsocket.send_string("test")
-
-@pytest.fixture
-async def quitter(sockets):
-    """
-    Set up a fake server to publish messages for testing.
-    """
-    pubsocket = sockets[1]  # output socket
-    yield pubsocket.send_string("QUIT")
+    yield (ctrl_socket, out_socket)
 
 @pytest.fixture
 async def app(ports):
     mock = tui.TUI(*ports)
     yield mock
-    await asyncio.sleep(.1)  # give pending tasks a chance to cancel
+    await asyncio.sleep(.2)  # give pending tasks a chance to cancel
 
 
-async def test_console_panel_receives_broadcast(app, publish, logger):
+async def test_console_panel_receives_broadcast(app, sockets, logger):
     async with app.run_test() as pilot:
-        logger.info('console')
-        console = pilot.app.get_widget_by_id("console")
-        await publish
+        await sockets[1].send_string("received")
         await pilot.pause(0.1)
-        print(console.history) #== 'test'
-        assert False
+        console = pilot.app.get_widget_by_id("console")
+        console.history[0] == 'received'
 
-async def test_quit_from_socket(app, quitter):
+async def test_quit_from_socket(app, sockets):
     async with app.run_test() as pilot:
-        await asyncio.wait([quitter])
-        await pilot.pause(0.5)
-        print(pilot.app._running)
-        assert False
+        await sockets[1].send_string("QUIT")
+        await pilot.pause(0.1)
+        assert not pilot.app._running
 
 async def test_log_panel_receives_logging(app, logger):
     async with app.run_test() as pilot:
