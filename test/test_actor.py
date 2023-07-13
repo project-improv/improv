a396a2ca7dcf8046b41c5bf1c18ce61ecde5a27b
cyzhou1028 <christopher.y.zhou@duke.edu>
2023-07-12 22:25:41 -0500

    fixing confs

--- test/test_actor.py
+++ test/test_actor.py
@@ -96,7 +96,6 @@ def test_repr(example_string_links):
     act.setLinks(example_string_links)
     assert act.__repr__() == "Test: dict_keys(['1', '2', '3'])"
 
-@pytest.mark.skip(reason="GH actions debug") 
 def test_setStoreInterface(setup_store):
     """Tests if the store is started and linked with the actor."""
 
