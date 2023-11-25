7950414b7f42a8ac0c7ffd8c9e22f57e018d5ba9
John Pearson <jmxpearson@gmail.com>
2023-11-24 21:45:55 -0500

    wait only

--- test/test_actor.py
+++ test/test_actor.py
@@ -30,7 +30,7 @@ def setup_store(set_store_loc, scope="module"):
 
     print("about to wait: first time")
     print("about to kill")
-    p.terminate()
+    # p.kill()
     print("about to wait")
     p.wait(10)
 
