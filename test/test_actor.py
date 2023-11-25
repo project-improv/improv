37dfecd540a9e2362bed69dfc90379a00fa8fdf6
John Pearson <jmxpearson@gmail.com>
2023-11-25 12:39:50 -0500

    try build pyarrow from source

--- test/test_actor.py
+++ test/test_actor.py
@@ -132,8 +132,6 @@ def test_foo(setup_store, set_store_loc):
     # act = Actor("Acquirer", set_store_loc)
     LOGGER.info(f"store_loc: {set_store_loc}")
     store = StoreInterface(store_loc=set_store_loc)
-    import time
-    time.sleep(5)
 
 
 @pytest.mark.parametrize(
