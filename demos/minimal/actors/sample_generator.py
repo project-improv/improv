7b4e14524a11b112f8a1deb75e753bfa959a0c43
Caitlin Lewis <clewis7@email.unc.edu>
2024-01-08 13:24:25 -0500

    update fpl demo as a sample_actor

--- demos/minimal/actors/sample_generator.py
+++ demos/minimal/actors/sample_generator.py
@@ -10,7 +10,8 @@ logger.setLevel(logging.INFO)
 class Generator(Actor):
     """Sample actor to generate data to pass into a sample processor.
 
-    Intended for use along with sample_processor.py.
+    Intended for use along with sample_processor.py. For visualization using `fastplotlib`,
+    see /demos/sample_actors/visual/.
     """
 
     def __init__(self, *args, **kwargs):
@@ -23,46 +24,38 @@ class Generator(Actor):
         return f"Name: {self.name}, Data: {self.data}"
 
     def setup(self):
-        """Generates an array that serves as an initial source of data.
-
-        Initial array is a 100 row, 5 column numpy matrix that contains
-        integers from 1-99, inclusive.
-        """
-
-        logger.info("Beginning setup for Generator")
-        self.data = np.asmatrix(np.random.randint(100, size=(100, 5)))
         logger.info("Completed setup for Generator")
 
     def stop(self):
-        """Save current randint vector to a file."""
+        """Save current cosine or sine wave vector to a file."""
 
         logger.info("Generator stopping")
         np.save("sample_generator_data.npy", self.data)
         return 0
 
     def runStep(self):
-        """Generates additional data after initial setup data is exhausted.
-
-        Data is of a different form as the setup data in that although it is
-        the same size (5x1 vector), it is uniformly distributed in [1, 10]
-        instead of in [1, 100]. Therefore, the average over time should
-        converge to 5.5.
+        """Generates a sine or cosine wave based on the frame_num and puts it into a queue
+        for the processor to take.
         """
+        xs = np.linspace(-10, 10, 100)
+        if self.frame_num % 2 == 0:
+            ys = np.cos(xs)
+            self.data = np.dstack((xs, ys))[0]
+        else:
+            ys = np.sin(xs)
+            self.data = np.dstack((xs, ys))[0]
 
-        if self.frame_num < np.shape(self.data)[0]:
-            data_id = self.client.put(
-                self.data[self.frame_num], str(f"Gen_raw: {self.frame_num}")
+        # put current data in the data store
+        data_id = self.client.put(
+                self.data.ravel(), str(f"Gen_raw: {self.frame_num}")
             )
             # logger.info('Put data in store')
-            try:
-                self.q_out.put([[data_id, str(self.frame_num)]])
-                logger.info("Sent message on")
-                self.frame_num += 1
-            except Exception as e:
-                logger.error(
-                    f"--------------------------------Generator Exception: {e}"
-                )
-        else:
-            self.data = np.concatenate(
-                (self.data, np.asmatrix(np.random.randint(10, size=(1, 5)))), axis=0
+        try:
+            self.q_out.put([[data_id, str(self.frame_num)]])
+            logger.info("Sent message on")
+            self.frame_num += 1
+        except Exception as e:
+            logger.error(
+                f"--------------------------------Generator Exception: {e}"
             )
+
