60c0a805eeec73117ede93f164811367e1cbac5c
Richard Schonberg <richardschonberg@Richards-MacBook-Pro.local>
2024-01-07 23:18:01 -0800

    comment out logging lines

--- demos/minimal/actors/sample_processor.py
+++ demos/minimal/actors/sample_processor.py
@@ -59,8 +59,8 @@ class Processor(Actor):
                 self.frame = self.client.get(frame)
             avg = np.mean(self.frame[0])
 
-            logger.info(f"Average: {avg}")
+            #logger.info(f"Average: {avg}")
             self.avg_list.append(avg)
-            logger.info(f"Overall Average: {np.mean(self.avg_list)}")
-            logger.info(f"Frame number: {self.frame_num}")
+            #logger.info(f"Overall Average: {np.mean(self.avg_list)}")
+            #logger.info(f"Frame number: {self.frame_num}")
             self.frame_num += 1
